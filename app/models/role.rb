9c439db20418da320d94c5ba3d467a8d2f6365a1
Milo Thurston <knirirr@gmail.com>
2015-09-25 11:55:21 +0100

    Removed erroneous commas.

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,7 +2,7 @@ class Role < ActiveRecord::Base
   has_many :users
 
   def self.roles
-    return %w{registered_user, admin, node_curator, curator, harvester}
+    return %w{registered_user admin node_curator curator harvester}
   end
 
 end
