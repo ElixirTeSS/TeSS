7d74f6c7cc88b29de3c52725c4ce779041e4af26
Aleksandra Nenadic <anenadic@manchester.ac.uk>
2015-11-11 23:34:45 +0000

    Added index for devise's confirmable.

--- db/migrate/20151029120731_add_devise_to_users.rb
+++ db/migrate/20151029120731_add_devise_to_users.rb
@@ -36,8 +36,8 @@ class AddDeviseToUsers < ActiveRecord::Migration
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
-    # add_index :users, :confirmation_token,   unique: true
-    # add_index :users, :unlock_token,         unique: true
+    add_index :users, :confirmation_token,   unique: true
+    add_index :users, :unlock_token,         unique: true
   end
 
   def self.down
--- db/schema.rb
+++ db/schema.rb
@@ -115,9 +115,11 @@ ActiveRecord::Schema.define(version: 20151029120731) do
   end
 
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", using: :btree
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["role_id"], name: "index_users_on_role_id", using: :btree
+  add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
   add_foreign_key "users", "roles"
