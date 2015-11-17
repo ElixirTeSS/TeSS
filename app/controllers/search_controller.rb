7663e81030e4254b6729031b9c4787ff8b76bb27
Niall Beard <niallbeard@gmail.com>
2015-11-17 20:45:20 +0000

    Made multiple model search more dynamic

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,15 +1,26 @@
 class SearchController < ApplicationController
 
+   @@search_models = %w(Material User Profile)
   # GET /searches
   # GET /searches.json
   def index 
     @results = {}
-    @results[:materials] = Material.search {fulltext search_params}.results
-    @results[:users] = User.search { fulltext search_params}.results
+    @@search_models.each do |model|
+       begin
+           @results[symbolize model] = model.constantize.search {fulltext search_params}
+       rescue
+         puts "Error finding results for #{model}"
+       end 
+    end
   end
 
   private
     # Never trust parameters from the scary internet, only allow the white list through.
+
+    def symbolize symbol
+       return symbol.downcase.pluralize.to_sym
+    end
+
     def search_params
       params[:q]
     end
