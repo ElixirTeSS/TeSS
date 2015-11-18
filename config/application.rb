7fb4dd6c43f904e2494d2f1cd0821662b7ab21f2
Aleksandra Nenadic <anenadic@manchester.ac.uk>
2015-11-18 11:49:44 +0000

    Renamed the TeSS app and added lib directory to autoload-paths.

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Tess
+module TeSS
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -22,5 +22,9 @@ module Tess
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    #Include lib/** files
+    config.autoload_paths += %W(#{config.root}/lib)
+
   end
 end
