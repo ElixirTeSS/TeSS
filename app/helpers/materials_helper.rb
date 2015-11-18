39fa7a669036bd8eeb79e63d004a8f8438535f46
Aleksandra Nenadic <anenadic@manchester.ac.uk>
2015-11-18 11:53:09 +0000

    Added helper method for getting licences list in views.

--- app/helpers/materials_helper.rb
+++ app/helpers/materials_helper.rb
@@ -1,2 +1,10 @@
 module MaterialsHelper
+  require File.expand_path(Rails.root.join('lib', 'tess', 'licence_dictionary.rb'))
+
+  # Returns an array of two-element arrays of licences ready to be used in options_for_select() for generating option/select tags
+  # [['Licence 1 full name','Licence 1 abbreviation'], ['Licence 2 full name','Licence 2 abbreviation'], ...]
+  def licence_options_for_select()
+    TeSS::LicenceDictionary.instance.licence_options_for_select
+  end
+
 end
