a17055c4c47a0d6c48b99820457d9fa6a4b41868
Niall Beard <niallbeard@gmail.com>
2015-11-24 11:09:44 +0000

    Tried to fix missing facets issue

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,11 +1,12 @@
 module SearchHelper
 	def filter_link name, value
 		new_parameter = {name => value}
-		parameters = request.parameters
+		parameters = request.parameters.clone
 
 		if parameters.include?(name) 
 			if !parameters[name].include?(value)
 		    	new_parameter = {name => [parameters.delete(name), value].flatten}
+		    	puts new_parameter
 		    else new_parameter = {}
 		    end
 		end
@@ -13,13 +14,14 @@ module SearchHelper
 	end
 
 	def remove_filter name, value
-	  parameters = Hash(request.parameters)
+	  parameters = request.parameters.clone
+	  
 	  if parameters.include?(name) 
 	  	if parameters[name].is_a?(Array)
 	  		parameters[name].delete(value)
 	  	else parameters.delete(name) 
 	  	end
-	  link_to 'x', parameters 
 	  end
+	  return link_to 'x', parameters 
 	end
 end
