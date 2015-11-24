5c76248d628653bdeef9a981ff18af48c1d05213
Milo Thurston <knirirr@gmail.com>
2015-11-24 13:54:54 +0000

    Hack to get flash colours working.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
 
   def flash_messages(opts = {})
     flash.each do |msg_type, message|
-      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in") do
+      concat(content_tag(:div, message, class: "alert alert-#{bootstrap_class_for(msg_type)} fade in") do
         concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
         concat message
       end)
