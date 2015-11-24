d45af8cd8462767977db75b026f8c846646dc1d7
Aleksandra Nenadic <anenadic@manchester.ac.uk>
2015-11-24 08:49:55 +0000

    More stuff on breadcrumbs 'stolen' from SEEK.

---
+++ lib/object.rb
@@ -0,0 +1,30 @@
+# From https://github.com/seek4science/seek/blob/master/lib/object.rb
+class Object
+  #instead of a and a.b and a.b.c and a.b.c.d?
+  #try_block {a.b.c.d?}
+  #in addition for being useful for nil's, works for any object that doesn't provide the required method
+  #so instead of a.respond_to? :b? and a.b? try_block { a.b? }
+  def try_block
+    puts "blah -----------"
+    yield
+  rescue NoMethodError, NameError => e
+    Rails.logger.error("Expected exception in try_block{} #{e}")
+    nil
+  rescue RuntimeError => e
+    Rails.logger.error("Expected exception in try_block{} #{e}")
+    if e.message.to_s == "Called id for nil, which would mistakenly be 4 -- if you really wanted the id of nil, use object_id"
+      nil
+    else
+      raise
+    end
+  end
+
+end
+
+Module.module_eval do
+  def class_alias_method_chain name, feature
+    singleton_class.instance_eval do
+      alias_method_chain name, feature
+    end
+  end
+end
