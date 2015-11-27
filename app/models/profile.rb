1352f8d38114e763666a2ac183e880bfb3f07658
Niall Beard <niallbeard@gmail.com>
2015-11-27 11:01:05 +0000

    Don't index persons just yet@

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,10 +1,12 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
   attr_accessor :email
-  searchable do 
+=begin
+  searchable do
     text :firstname
     text :surname
   end
+=end
   # TODO:
   # Add validations for these fields (default nil, except email)
   # firstname:text surname:text image_url:text email:text website:text
