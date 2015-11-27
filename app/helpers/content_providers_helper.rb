5802607502aadda5c9263aef7e3e05be706a7509
Niall Beard <niallbeard@gmail.com>
2015-11-27 17:20:24 +0000

    Added logo to materials display partial

--- app/helpers/content_providers_helper.rb
+++ app/helpers/content_providers_helper.rb
@@ -1,2 +1,7 @@
 module ContentProvidersHelper
+  def html_logo_for(content_provider)
+    unless content_provider.nil?
+      return link_to(image_tag(content_provider.logo_url, :class=>'content_provider_logo'), content_provider)
+    end
+  end
 end
