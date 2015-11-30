f92c4762d494a20aa401da2e6da929818b5c4b73
Niall Beard <niallbeard@gmail.com>
2015-11-30 13:44:18 +0000

    Return empty string rather than nil if there is no content provider for a material

--- app/helpers/content_providers_helper.rb
+++ app/helpers/content_providers_helper.rb
@@ -1,6 +1,8 @@
 module ContentProvidersHelper
   def html_logo_for(content_provider)
-    unless content_provider.nil?
+    if content_provider.nil?
+      return '' #Maybe add default placeholder logo
+    else
       return link_to(image_tag(content_provider.logo_url, :class=>'content_provider_logo'), content_provider)
     end
   end
