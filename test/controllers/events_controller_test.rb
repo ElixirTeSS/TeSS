c00c3fa59d6f28d3530bac89ac008723a141f7e0
Niall Beard <niallbeard@gmail.com>
2015-11-30 13:20:54 +0000

    Added login line to restricted methods

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class EventsControllerTest < ActionController::TestCase
+
   setup do
     @event = events(:one)
   end
@@ -12,11 +13,13 @@ class EventsControllerTest < ActionController::TestCase
   end
 
   test "should get new" do
+    sign_in users(:regular_user)
     get :new
     assert_response :success
   end
 
   test "should create event" do
+    sign_in users(:regular_user)
     assert_difference('Event.count') do
       post :create, event: { category: @event.category, city: @event.city, country: @event.country, county: @event.county, description: @event.description, end: @event.end, field: @event.field, id: @event.id, latitude: @event.latitude, link: @event.link, longitude: @event.longitude, postcode: @event.postcode, provider: @event.provider, sponsor: @event.sponsor, start: @event.start, subtitle: @event.subtitle, title: @event.title, venue: @event.venue }
     end
@@ -30,16 +33,19 @@ class EventsControllerTest < ActionController::TestCase
   end
 
   test "should get edit" do
+    sign_in users(:regular_user)
     get :edit, id: @event
     assert_response :success
   end
 
   test "should update event" do
+    sign_in users(:regular_user)
     patch :update, id: @event, event: { category: @event.category, city: @event.city, country: @event.country, county: @event.county, description: @event.description, end: @event.end, field: @event.field, id: @event.id, latitude: @event.latitude, link: @event.link, longitude: @event.longitude, postcode: @event.postcode, provider: @event.provider, sponsor: @event.sponsor, start: @event.start, subtitle: @event.subtitle, title: @event.title, venue: @event.venue }
     assert_redirected_to event_path(assigns(:event))
   end
 
   test "should destroy event" do
+    sign_in users(:regular_user)
     assert_difference('Event.count', -1) do
       delete :destroy, id: @event
     end
--- test/controllers/packages_controller_test.rb
+++ test/controllers/packages_controller_test.rb
@@ -12,11 +12,13 @@ class PackagesControllerTest < ActionController::TestCase
   end
 
   test "should get new" do
+    sign_in users(:regular_user)
     get :new
     assert_response :success
   end
 
   test "should create package" do
+    sign_in users(:regular_user)
     assert_difference('Package.count') do
       post :create, package: { description: @package.description, image_url: @package.image_url, name: @package.name, public: @package.public }
     end
@@ -30,16 +32,19 @@ class PackagesControllerTest < ActionController::TestCase
   end
 
   test "should get edit" do
+    sign_in users(:regular_user)
     get :edit, id: @package
     assert_response :success
   end
 
   test "should update package" do
+    sign_in users(:regular_user)
     patch :update, id: @package, package: { description: @package.description, image_url: @package.image_url, name: @package.name, public: @package.public }
     assert_redirected_to package_path(assigns(:package))
   end
 
   test "should destroy package" do
+    sign_in users(:regular_user)
     assert_difference('Package.count', -1) do
       delete :destroy, id: @package
     end
