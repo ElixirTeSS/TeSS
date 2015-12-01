c5da716fd62c09e6a8abe613d9645782d3b911ef
Niall Beard <niallbeard@gmail.com>
2015-12-01 17:19:27 +0000

    Added sort by dropdown to events

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,32 @@
 //= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .
+
+function redirect_to_sort_url(){
+    window.location.replace(
+        updateURLParameter(
+            window.location.href,
+            'sort',
+            $('#sort').find(":selected").val()
+        )
+    )
+}
+
+function updateURLParameter(url, param, paramVal){
+    var newAdditionalURL = "";
+    var tempArray = url.split("?");
+    var baseURL = tempArray[0];
+    var additionalURL = tempArray[1];
+    var temp = "";
+    if (additionalURL) {
+        tempArray = additionalURL.split("&");
+        for (i=0; i<tempArray.length; i++){
+            if(tempArray[i].split('=')[0] != param){
+                newAdditionalURL += temp + tempArray[i];
+                temp = "&";
+            }
+        }
+    }
+    var rows_txt = temp + "" + param + "=" + paramVal;
+    return baseURL + "?" + newAdditionalURL + rows_txt;
+}
