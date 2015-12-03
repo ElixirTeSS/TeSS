23e10672046a527bf0b035b04c695571afbc64e1
Milo Thurston <knirirr@gmail.com>
2015-12-03 11:45:48 +0000

    Add roles for new installation.

--- app/models/role.rb
+++ app/models/role.rb
@@ -5,4 +5,16 @@ class Role < ActiveRecord::Base
     return %w{registered_user admin node_curator curator harvester}
   end
 
+  # Use this with Role.create_roles on a new installation
+  # to set the inital roles up.
+  def self.create_roles
+    self.roles.each do |name|
+      r = Role.find_by_name(name)
+      if r == nil
+        r = Role.new(:name => name)
+        r.save!
+      end
+    end
+  end
+
 end
