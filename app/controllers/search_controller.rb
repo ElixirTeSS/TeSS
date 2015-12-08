b9bb540e29e68059d12d9bb89f99882039ea148f
Niall Beard <niallbeard@gmail.com>
2015-12-08 15:00:26 +0000

    Added tabs for global search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -14,6 +14,7 @@ class SearchController < ApplicationController
          puts "Error finding results for #{model}"
        end 
     end
+    @results.reject!{|k,result| result.total < 1}
   end
 
   private
