0af609ddc079bc4187b53a4420016c56d77fbc0d
Niall Beard <niallbeard@gmail.com>
2015-12-17 14:15:34 +0000

    Added activity log page to materials

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,26 @@
 class ActivitiesController < ApplicationController
+
+  before_action :set_resource, only: [:show]
+
+  @@models = %w( content_provider material )
+
   def index
     @activities = PublicActivity::Activity.order("created_at desc")
+  end
+
+  def show
+  end
+
+  private
+  def set_resource
+    params.permit(@@models)
+
+    @@models.each do |model|
+      parameter_name = model+'_id'
+      if params.include?(parameter_name)
+        resource_model = model.camelcase.constantize
+        @resource = resource_model.friendly.find(params[parameter_name])
+      end
+    end
   end
 end
