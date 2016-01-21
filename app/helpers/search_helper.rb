f49ccfc2d1a716aa9c57d5f15c5a6bf260497307
Niall Beard <niallbeard@gmail.com>
2016-01-21 11:17:32 +0000

    Fixed issue of selecting lower parameters removing existing params
    Strange bug but modifications to a variable that was a duplication of request.params (using .dup) still affected the original request.params causing .delete to remove from the original. No idea why that is but using params rather than request.params does not have that behaviour

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,11 +1,12 @@
 module SearchHelper
+
 	def filter_link name, value
 		new_parameter = {name => value}
-		parameters = request.parameters.clone
+		parameters = params.dup
 		#if there's already a filter of the same facet type, create/add to an array
-		if parameters.include?(name) 
+		if parameters.include?(name)
 			if !parameters[name].include?(value)
-		    	new_parameter = {name => [parameters.delete(name), value].flatten}
+		    	new_parameter = {name => [parameters.dup.delete(name), value].flatten}
 		    else new_parameter = {}
 		    end
 		end
@@ -14,28 +15,28 @@ module SearchHelper
 		return link_to value, parameters.merge(new_parameter)
 	end
 
+
 	def remove_filter_link name, value
-	  parameters = request.parameters.clone
-	  
+		parameters = params.dup
 	  #delete a filter from an array or delete the whole facet if it is the only one
-	  if parameters.include?(name) 
+	  if parameters.include?(name)
 	  	if parameters[name].is_a?(Array)
 	  		parameters[name].delete(value)
-	  	else parameters.delete(name) 
+	  	else parameters.delete(name)
 	  	end
-	  end
+		end
 	  #remove the page option if it exists
-      parameters.delete('page')
+		parameters.delete('page')
 	  return link_to "x #{value}", parameters 
 	end
 
 	def show_more_link facet
-		parameters = request.parameters.clone
+		parameters = params.dup
 		return link_to "Show more #{facet.humanize.pluralize}", parameters.merge("#{facet}_all"=>true)
 	end
 
 	def show_less_link facet
-		parameters = request.parameters.clone
+		parameters = params.dup
 		parameters.delete("#{facet}_all")
 		return link_to "Show less #{facet.humanize.pluralize}", parameters
 	end
