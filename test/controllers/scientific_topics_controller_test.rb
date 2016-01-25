ac6acc1303c92128fe80231b5798212f0336698d
Niall Beard <niallbeard@gmail.com>
2016-01-25 12:04:24 +0000

    Added sign-in in tests to reflect auth structure

--- test/controllers/scientific_topics_controller_test.rb
+++ test/controllers/scientific_topics_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ScientificTopicsControllerTest < ActionController::TestCase
   include Devise::TestHelpers
-  
+
   setup do
     @scientific_topic = scientific_topics(:one)
   end
@@ -14,11 +14,13 @@ class ScientificTopicsControllerTest < ActionController::TestCase
   end
 
   test "should get new" do
+    sign_in users(:regular_user)
     get :new
     assert_response :success
   end
 
   test "should create scientific_topic" do
+    sign_in users(:regular_user)
     assert_difference('ScientificTopic.count') do
       post :create, scientific_topic: { consider: @scientific_topic.consider, created_in: @scientific_topic.created_in, definitions: @scientific_topic.definitions, documentation: @scientific_topic.documentation, has_alternative_id: @scientific_topic.has_alternative_id, has_broad_synonym: @scientific_topic.has_broad_synonym, has_dbxref: @scientific_topic.has_dbxref, has_definition: @scientific_topic.has_definition, has_exact_synonym: @scientific_topic.has_exact_synonym, has_related_synonym: @scientific_topic.has_related_synonym, has_subset: @scientific_topic.has_subset, obsolete: @scientific_topic.obsolete, obsolete_since: @scientific_topic.obsolete_since, parents: @scientific_topic.parents, preferred_label: @scientific_topic.preferred_label, prefix_iri: @scientific_topic.prefix_iri, replaced_by: @scientific_topic.replaced_by, saved_by: @scientific_topic.saved_by, subset_property: @scientific_topic.subset_property, synonyms: @scientific_topic.synonyms }
     end
@@ -32,16 +34,19 @@ class ScientificTopicsControllerTest < ActionController::TestCase
   end
 
   test "should get edit" do
+    sign_in users(:regular_user)
     get :edit, id: @scientific_topic
     assert_response :success
   end
 
   test "should update scientific_topic" do
+    sign_in users(:regular_user)
     patch :update, id: @scientific_topic, scientific_topic: { consider: @scientific_topic.consider, created_in: @scientific_topic.created_in, definitions: @scientific_topic.definitions, documentation: @scientific_topic.documentation, has_alternative_id: @scientific_topic.has_alternative_id, has_broad_synonym: @scientific_topic.has_broad_synonym, has_dbxref: @scientific_topic.has_dbxref, has_definition: @scientific_topic.has_definition, has_exact_synonym: @scientific_topic.has_exact_synonym, has_related_synonym: @scientific_topic.has_related_synonym, has_subset: @scientific_topic.has_subset, obsolete: @scientific_topic.obsolete, obsolete_since: @scientific_topic.obsolete_since, parents: @scientific_topic.parents, preferred_label: @scientific_topic.preferred_label, prefix_iri: @scientific_topic.prefix_iri, replaced_by: @scientific_topic.replaced_by, saved_by: @scientific_topic.saved_by, subset_property: @scientific_topic.subset_property, synonyms: @scientific_topic.synonyms }
     assert_redirected_to scientific_topic_path(assigns(:scientific_topic))
   end
 
   test "should destroy scientific_topic" do
+    sign_in users(:regular_user)
     assert_difference('ScientificTopic.count', -1) do
       delete :destroy, id: @scientific_topic
     end
