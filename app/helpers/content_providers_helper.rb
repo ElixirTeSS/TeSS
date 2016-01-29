7d456771358a8eeb625090ae7a2dfdbd0f808550
Aleksandra Nenadic <anenadic@manchester.ac.uk>
2016-01-29 14:51:00 +0000

    Styled provider's logo on materials index.

--- app/helpers/content_providers_helper.rb
+++ app/helpers/content_providers_helper.rb
@@ -1,9 +1,9 @@
 module ContentProvidersHelper
-  def html_logo_for(content_provider)
+  def html_logo_for(content_provider, html_class=nil)
     if content_provider.blank?
       return ''
     else
-      return link_to(image_tag(get_content_provider_logo_url(content_provider), :class=>'content_provider_logo'), content_provider)
+      return link_to(image_tag(get_content_provider_logo_url(content_provider), :class=>html_class), content_provider)
     end
   end
 end
