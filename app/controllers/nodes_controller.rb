91331b9690672a809391f934dbdf1b329456c0e9
Aleksandra Nenadic <anenadic@manchester.ac.uk>
2016-02-01 16:26:35 +0000

    Initial style for nodes index page. Added support for breadcrumbs too.

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -1,6 +1,8 @@
 class NodesController < ApplicationController
   before_action :set_node, only: [:show, :edit, :update, :destroy]
 
+  include TeSS::BreadCrumbs
+
   # GET /nodes
   # GET /nodes.json
   def index
