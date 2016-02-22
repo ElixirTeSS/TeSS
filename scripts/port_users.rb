d04f2c427ce25a326e5ddeb2d31896e63450ba35
Milo Thurston <knirirr@gmail.com>
2016-02-22 16:01:59 +0000

    Script to port users over.

---
+++ scripts/port_users.rb
@@ -0,0 +1,36 @@
+# This script expects a users.csv file in the format:
+# username | email address | full name
+# The full name will have to be split to get the firstname/surname used in the
+# profile for the current version of tess.
+# Run this script with:
+# rails runner -e development scripts/port_users.rb
+
+IO.readlines("#{Rails.root}/scripts/users.csv").each do |line|
+  parts = line.split('|').map(&:strip)
+  username = parts[0]
+  email = parts[1]
+  if !parts[2].nil?
+    nameparts = parts[2].split(/\s+/).map(&:strip)
+    if nameparts.length == 2
+      firstname = nameparts[0]
+      lastname = nameparts[1]
+    elsif nameparts.length == 3
+      firstname = nameparts[0]
+      lastname = nameparts[2]
+    end
+  end
+  #puts "#{firstname},#{lastname},#{username},#{email}"
+  u = User.find_by_username(username)
+  if u.nil?
+    u = User.new(:username => username, :email => email)
+    u.set_default_profile
+    u.set_default_role
+    u.authentication_token = Devise.friendly_token
+    u.password = Devise.friendly_token
+    u.profile.firstname = firstname
+    u.profile.surname = lastname
+    u.save!
+  end
+
+
+end
