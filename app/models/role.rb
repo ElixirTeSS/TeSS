8c42f68bec0b8515daf6b5d8212059a451e25837
Aleksandra Nenadic <anenadic@manchester.ac.uk>
2016-03-01 09:49:07 +0000

    Renamed 'harverster' role to 'api_user' as we may have regualar users using the API too.

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,11 +2,11 @@ class Role < ActiveRecord::Base
   has_many :users
 
   def self.roles
-    return %w{registered_user admin node_curator curator harvester}
+    return %w{registered_user admin node_curator curator api_user}
   end
 
   # Use this with Role.create_roles on a new installation
-  # to set the inital roles up.
+  # to set the initial roles up.
   def self.create_roles
     self.roles.each do |name|
       r = Role.find_by_name(name)
