ce9d1b411c9efdab67d129bf619e02bcc824cbf7
Aleksandra Nenadic <anenadic@manchester.ac.uk>
2016-03-04 10:34:16 +0000

    Modification for devise's rendering of error messages on login form.

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,2 +1,23 @@
-class DeviseHelper
+module DeviseHelper
+  def devise_error_messages!
+    return "" if resource.errors.empty?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    sentence = I18n.t("errors.messages.not_saved",
+                      count: resource.errors.count,
+                      resource: resource.class.model_name.human.downcase)
+
+    html = <<-HTML
+    <div id="error_expl" class="panel panel-danger">
+      <div class="panel-heading">
+      <h3 class="panel-title">#{sentence}</h3>
+      </div>
+      <div class="panel-body">
+      <ul>#{messages}</ul>
+      </div>
+    </div>
+    HTML
+
+    html.html_safe
+  end
 end
