1d0bd473edf494c117c7586f04059feb3f6f9ecf
Aleksandra Nenadic <anenadic@manchester.ac.uk>
2016-04-06 10:05:26 +0100

    Fix for users breadcrumbs - it now displays username instead of id.

--- lib/tess/bread_crumbs.rb
+++ lib/tess/bread_crumbs.rb
@@ -37,8 +37,8 @@ module TeSS
     end
 
     def add_show_breadcrumb resource, breadcrumb_name=nil
-        breadcrumb_name ||= "#{resource.respond_to?(:title) ? resource.title : resource.respond_to?(:name) ? resource.name : resource.id}"
-        add_breadcrumb breadcrumb_name, url_for(:controller => resource.class.name.underscore.pluralize, :action => 'show', :id => resource.id)
+        breadcrumb_name ||= "#{resource.respond_to?(:title) ? resource.title : resource.respond_to?(:name) ? resource.name : resource.respond_to?(:username) ? resource.username : resource.id}"
+        add_breadcrumb breadcrumb_name, url_for(:controller => resource.class.name.underscore.pluralize, :action => 'show', :id => (resource.respond_to?(:username) ? resource.username: resource.id))
     end
 
     def add_edit_breadcrumb resource, breadcrumb_name=nil
