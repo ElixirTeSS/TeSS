ace3cf2e216a5267a9180c7838fa5430bfbb7764
Aleksandra Nenadic <anenadic@manchester.ac.uk>
2016-04-07 21:25:27 +0100

    Passing changes to user's profile as a Proc to devise's create method so we can now call super instead of modifying create method inline.

--- app/controllers/tess_devise/registrations_controller.rb
+++ app/controllers/tess_devise/registrations_controller.rb
@@ -2,31 +2,13 @@ class TessDevise::RegistrationsController < Devise::RegistrationsController
   # Inspired by http://stackoverflow.com/questions/3546289/override-devise-registrations-controller
 
   def create
-    build_resource(sign_up_params)
-
-    # Add account email as public email in profile
-    if params[:make_email_public]
-      resource.profile = Profile.new(email: resource.email)
-      # resource.profile.email = resource.email
+    add_email_to_profile_proc = Proc.new do |res|
+        if params[:make_email_public]
+          res.profile.update_attribute(:email, res.unconfirmed_email)
+        end
     end
-
-    resource.save
-    yield resource if block_given?
-    if resource.persisted?
-      if resource.active_for_authentication?
-        set_flash_message :notice, :signed_up if is_flashing_format?
-        sign_up(resource_name, resource)
-        respond_with resource, location: after_sign_up_path_for(resource)
-      else
-        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
-        expire_data_after_sign_in!
-        respond_with resource, location: after_inactive_sign_up_path_for(resource)
-      end
-    else
-      clean_up_passwords resource
-      set_minimum_password_length
-      respond_with resource
-    end
+    # call parent's create method and pass the proc to modify the profile's email address
+    super &add_email_to_profile_proc
   end
 
   # Set the after update path to be user's show page
