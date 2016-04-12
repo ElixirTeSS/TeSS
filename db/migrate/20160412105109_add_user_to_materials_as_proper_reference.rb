5c1cc67a278d5c024ae72985cf0c15db1ba7dec4
Aleksandra Nenadic <anenadic@manchester.ac.uk>
2016-04-12 13:11:47 +0100

    Fix for user_id column on materials to be proper rails-style reference (issue #149).

---
+++ db/migrate/20160412105109_add_user_to_materials_as_proper_reference.rb
@@ -0,0 +1,9 @@
+class AddUserToMaterialsAsProperReference < ActiveRecord::Migration
+  def change
+    # Remove the old user_id column
+    remove_column :materials, :user_id, :integer
+    # Create new 'proper' association (this will recreate user_id column (also foreign key as above),
+    # but would also create methods such as material.user, etc.)
+    add_reference :materials, :user, index: true, foreign_key: true
+  end
+end
