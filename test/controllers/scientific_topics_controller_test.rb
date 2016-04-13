8c235e02ccf456638abcf604b6fa4336c911832b
Niall Beard <niallbeard@gmail.com>
2016-04-13 15:55:11 +0100

    Should not be able to edit or delete as soley populated from EDAM

--- test/controllers/scientific_topics_controller_test.rb
+++ test/controllers/scientific_topics_controller_test.rb
@@ -36,21 +36,34 @@ class ScientificTopicsControllerTest < ActionController::TestCase
   test "should get edit" do
     sign_in users(:regular_user)
     get :edit, id: @scientific_topic
-    assert_response :success
+    assert_response :forbidden
+
+    sign_in users(:admin)
+    get :edit, id: @scientific_topic
+    assert_response :forbidden
   end
 
   test "should update scientific_topic" do
     sign_in users(:regular_user)
     patch :update, id: @scientific_topic, scientific_topic: { consider: @scientific_topic.consider, created_in: @scientific_topic.created_in, definitions: @scientific_topic.definitions, documentation: @scientific_topic.documentation, has_alternative_id: @scientific_topic.has_alternative_id, has_broad_synonym: @scientific_topic.has_broad_synonym, has_dbxref: @scientific_topic.has_dbxref, has_definition: @scientific_topic.has_definition, has_exact_synonym: @scientific_topic.has_exact_synonym, has_related_synonym: @scientific_topic.has_related_synonym, has_subset: @scientific_topic.has_subset, obsolete: @scientific_topic.obsolete, obsolete_since: @scientific_topic.obsolete_since, parents: @scientific_topic.parents, preferred_label: @scientific_topic.preferred_label, prefix_iri: @scientific_topic.prefix_iri, replaced_by: @scientific_topic.replaced_by, saved_by: @scientific_topic.saved_by, subset_property: @scientific_topic.subset_property, synonyms: @scientific_topic.synonyms }
-    assert_redirected_to scientific_topic_path(assigns(:scientific_topic))
+    assert_response :forbidden
+
+    sign_in users(:admin)
+    patch :update, id: @scientific_topic, scientific_topic: { consider: @scientific_topic.consider, created_in: @scientific_topic.created_in, definitions: @scientific_topic.definitions, documentation: @scientific_topic.documentation, has_alternative_id: @scientific_topic.has_alternative_id, has_broad_synonym: @scientific_topic.has_broad_synonym, has_dbxref: @scientific_topic.has_dbxref, has_definition: @scientific_topic.has_definition, has_exact_synonym: @scientific_topic.has_exact_synonym, has_related_synonym: @scientific_topic.has_related_synonym, has_subset: @scientific_topic.has_subset, obsolete: @scientific_topic.obsolete, obsolete_since: @scientific_topic.obsolete_since, parents: @scientific_topic.parents, preferred_label: @scientific_topic.preferred_label, prefix_iri: @scientific_topic.prefix_iri, replaced_by: @scientific_topic.replaced_by, saved_by: @scientific_topic.saved_by, subset_property: @scientific_topic.subset_property, synonyms: @scientific_topic.synonyms }
+    assert_response :forbidden
   end
 
-  test "should destroy scientific_topic" do
+  test "should not destroy scientific_topic" do
     sign_in users(:regular_user)
-    assert_difference('ScientificTopic.count', -1) do
+    assert_no_difference('ScientificTopic.count') do
       delete :destroy, id: @scientific_topic
     end
+    assert_response :forbidden
 
-    assert_redirected_to scientific_topics_path
+    sign_in users(:admin)
+    assert_no_difference('ScientificTopic.count') do
+      delete :destroy, id: @scientific_topic
+    end
+    assert_response :forbidden
   end
 end
