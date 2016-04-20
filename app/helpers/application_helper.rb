7f76aab5eefe5ee80c40d24267a7bcf5a8a33cdb
Aleksandra Nenadic <anenadic@manchester.ac.uk>
2016-04-20 10:08:11 +0100

    Style tweak to announcement and flash messages.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,7 +18,7 @@ module ApplicationHelper
 
   def flash_messages(opts = {})
     flash.each do |msg_type, message|
-      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in", style: "font-size: 150%;") do
+      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in", style: "font-size: 120%; font-weight: bold;") do
         concat content_tag(:button, '&times;'.html_safe, class: "close", data: { dismiss: 'alert' }, "aria-label" => 'close')
         concat message
       end)
