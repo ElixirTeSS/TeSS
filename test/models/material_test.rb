7efb0b11112d5c8e3d191fa6986fb7337b01b2a4
Niall Beard <niallbeard@gmail.com>
2016-04-26 18:09:34 +0100

    Added test to check material owner gets re-assigned when owner deleted

--- test/models/material_test.rb
+++ test/models/material_test.rb
@@ -5,18 +5,47 @@ class MaterialTest < ActiveSupport::TestCase
   #   assert true
   # end
 
-  test "material attributes title, short_decription and url should not be empty" do
-    material = Material.new
+
+  setup do
+    @user = User.new(:username=>'bobo',
+                  :email=>'exampl@example.com',
+                  :role => Role.first,
+                  :password => SecureRandom.base64
+    )
+    @user.save!
+    @material = Material.new(:title => 'title', :short_description => 'short desc', :url => 'http://goog.e.com', :user => @user)
+    @material.save!
+  end
+
+  test 'should reassign owner when user deleted' do
+    material_id = @material.id
+    owner = @material.user
+    assert_not_equal 'default_user', owner.role.name
+    owner.destroy
+    #Reload the material
+    material = Material.find_by_id(material_id)
+    assert_equal 'default_user', material.user.role.name
+  end
+
+=begin
+  test 'should save good new material' do
+    material = new_material()
+    assert material.valid?
+    assert materials.errors.empty?
+  end
+
+  test 'url should not be empty' do
+    material = new_material('')
     assert material.invalid?
-    assert material.errors[:title].any?
-    assert material.errors[:short_description].any?
     assert material.errors[:url].any?
   end
 
-  def new_material(material_url)
-    Material.new(title: materials(:good_material).title,
-        short_description: materials(:good_material).short_description,
-        url: material_url)
+  def new_material(title=materials(:good_material).title,
+                   short_description=materials(:good_material).short_description,
+                   url=materials(:good_material).url)
+    Material.new(title: title,
+        short_description: short_description,
+        url: url)
   end
 
   test "url must be in correct format" do
@@ -28,5 +57,6 @@ class MaterialTest < ActiveSupport::TestCase
     material2 =  new_material(materials(:good_material).url)
     assert material2.valid?, "#{material.url} should be valid"
   end
+=end
 
 end
