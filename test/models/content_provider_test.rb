3789f1c632dba28226699129e88c88f5d530fc9a
Aleksandra Nenadic <a.nenadic@manchester.ac.uk>
2016-05-26 16:52:55 +0100

    Fix for content providers' tests for image_url.

--- test/models/content_provider_test.rb
+++ test/models/content_provider_test.rb
@@ -4,11 +4,21 @@ class ContentProviderTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-  test 'should not save invalid url for image_url' do
+  test 'should not save invalid content provider' do
     content_provider = ContentProvider.new
     assert content_provider.invalid?
     assert content_provider.errors[:title].any?
     assert content_provider.errors[:url].any?
+  end
+
+  test 'should allow empty image_url' do
+    content_provider = content_providers(:provider_with_empty_image_url)
+    assert content_provider.valid?
+  end
+
+  test 'should not allow invalid image_url' do
+    content_provider = content_providers(:provider_with_invalid_image_url)
+    assert content_provider.invalid?
     assert content_provider.errors[:image_url].any?
   end
 
