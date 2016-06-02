09e852f3ed6fdf03eed4f66678ad65d6fb84c3b0
Aleksandra Nenadic <a.nenadic@manchester.ac.uk>
2016-06-02 11:10:06 +0100

    Added node info for content provider in view. Issue #243.

--- app/controllers/content_providers_controller.rb
+++ app/controllers/content_providers_controller.rb
@@ -102,7 +102,7 @@ class ContentProvidersController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def content_provider_params
-    params.require(:content_provider).permit(:title, :url, :image_url, :description, :id,
+    params.require(:content_provider).permit(:title, :url, :image_url, :description, :id, :node_id,
                                              {:keywords => []}, :remote_updated_date, :remote_created_date, :local_updated_date, :remote_updated_date)
   end
 end
