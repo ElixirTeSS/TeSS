e265dbe28ffff52323ef6b8439cb251ccee77642
Finn Bacall <finn.bacall@manchester.ac.uk>
2016-06-13 14:24:59 +0100

    Basic index page and forms for workflows. #258

--- app/helpers/workflows_helper.rb
+++ app/helpers/workflows_helper.rb
@@ -1,6 +1,6 @@
 module WorkflowsHelper
 
   WORKFLOWS_INFO = "Training workflows in TeSS are visual, step-by-step protocols that allow users to navigate materials in TeSS in topic- or task-related ways.\n\n"+
-
-      "For example, they may encapsulate typical stages of particular kinds of data analysis (protein sequence analysis, structure analysis, etc.), where each stage/node in the workflow represents a given analysis step and links to the corresponding training resources."
+      "For example, they may encapsulate typical stages of particular kinds of data analysis (protein sequence analysis, structure analysis, etc.), where each stage/node in the workflow represents a given analysis step and links to the corresponding training resources.\n\n"+
+      "You can see examples of how Training Workflows will be implemented on the <a href='https://tess-old.elixir-uk.org/workflow/25c37b3b-f88a-42b1-a23e-a7563102823c'> old TeSS Pilot site</a> (now defunct)."
 end
