167a456c80805cafeba7408454b729f72944b829
Finn Bacall <finn.bacall@manchester.ac.uk>
2016-06-30 15:47:07 +0100

    Some workflow model tests

--- test/models/workflow_test.rb
+++ test/models/workflow_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class WorkflowTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "can create workflow" do
+    workflow = Workflow.new(title: 'hello world')
+
+    assert workflow.save
+  end
+
+  test "can't create workflow without title" do
+    workflow = Workflow.new
+
+    refute workflow.save
+    assert_not_empty workflow.errors
+  end
+
 end
