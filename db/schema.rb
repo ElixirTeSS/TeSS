7db2c37e3eb9b63356ef6f30e6da90d6b6c63489
Finn Bacall <finn.bacall@manchester.ac.uk>
2016-07-20 17:10:50 +0100

    Forgot to migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160602122936) do
+ActiveRecord::Schema.define(version: 20160720160421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -164,7 +164,7 @@ ActiveRecord::Schema.define(version: 20160602122936) do
   create_table "packages", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.string   "image_url"
+    t.text     "image_url"
     t.boolean  "public",      default: true
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
