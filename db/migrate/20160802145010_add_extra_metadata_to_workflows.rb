0fd0212ba99f50a0b7045dc5be97912c3e75e7bf
Finn Bacall <finn.bacall@manchester.ac.uk>
2016-08-04 12:15:46 +0100

    Extra workflow metadata migration. #303

---
+++ db/migrate/20160802145010_add_extra_metadata_to_workflows.rb
@@ -0,0 +1,14 @@
+class AddExtraMetadataToWorkflows < ActiveRecord::Migration
+  def change
+    add_column :workflows, :target_audience, :string, array: true, default: []
+    add_column :workflows, :scientific_topic, :string, array: true, default: []
+    add_column :workflows, :keywords, :string, array: true, default: []
+    add_column :workflows, :authors, :string, array: true, default: []
+    add_column :workflows, :contributors, :string, array: true, default: []
+    add_column :workflows, :licence, :string
+    add_column :workflows, :difficulty_level, :string
+    add_column :workflows, :doi, :string
+    add_column :workflows, :remote_created_date, :date
+    add_column :workflows, :remote_updated_date, :date
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160722125757) do
+ActiveRecord::Schema.define(version: 20160802145010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -337,9 +337,19 @@ ActiveRecord::Schema.define(version: 20160722125757) do
     t.string   "description"
     t.integer  "user_id"
     t.json     "workflow_content"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
     t.string   "slug"
+    t.string   "target_audience",     default: [],              array: true
+    t.string   "scientific_topic",    default: [],              array: true
+    t.string   "keywords",            default: [],              array: true
+    t.string   "authors",             default: [],              array: true
+    t.string   "contributors",        default: [],              array: true
+    t.string   "licence"
+    t.string   "difficulty_level"
+    t.string   "doi"
+    t.date     "remote_created_date"
+    t.date     "remote_updated_date"
   end
 
   add_index "workflows", ["slug"], name: "index_workflows_on_slug", unique: true, using: :btree
