eba558ded3b32668a140eb0eecec8f6b52f9e68f
Finn Bacall <finn.bacall@manchester.ac.uk>
2016-09-05 13:37:20 +0100

    Prevent duplicate collaborations. #307

--- app/models/collaboration.rb
+++ app/models/collaboration.rb
@@ -3,4 +3,6 @@ class Collaboration < ActiveRecord::Base
   belongs_to :user
   belongs_to :resource, polymorphic: true
 
+  validates_uniqueness_of :user, scope: :resource, message: 'is already a collaborator'
+
 end
