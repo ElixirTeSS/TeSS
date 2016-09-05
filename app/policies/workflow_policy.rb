e3dd7771699dee6257b6521e95f9ba09e8aa7a53
Finn Bacall <finn.bacall@manchester.ac.uk>
2016-09-05 13:10:58 +0100

    Fixed problem with owners not being able to manage collaborations on their own resources. #307

--- app/policies/workflow_policy.rb
+++ app/policies/workflow_policy.rb
@@ -16,7 +16,7 @@ class WorkflowPolicy < ApplicationPolicy
     # Admin role can update/destroy any object, other roles can only update objects they created
     return true if @user.is_admin? # allow admin roles for all requests - UI and API
     if request_is_api?(@request) #is this an API action - allow api_user roles only
-      if @user.has_role?(:api_user) and @user.is_owner?(@record) # check ownership
+      if @user.has_role?(:api_user) or @user.is_owner?(@record) # check ownership
         return true
       else
         return false
