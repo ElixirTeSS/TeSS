647fa8d422281288d569935c6f132b01c4b5c66f
Finn Bacall <finn.bacall@manchester.ac.uk>
2016-09-06 15:28:11 +0100

    Don't try and log workflow diagram changes when there wasn't a diagram originally

--- app/models/workflow.rb
+++ app/models/workflow.rb
@@ -27,7 +27,7 @@ class Workflow < ActiveRecord::Base
 
   update_suggestions(:keywords, :contributors, :authors, :target_audience)
 
-  after_save :log_diagram_modification
+  after_update :log_diagram_modification
 
   def self.facet_fields
     %w( )
@@ -36,26 +36,28 @@ class Workflow < ActiveRecord::Base
   private
 
   def log_diagram_modification
-    old_nodes = workflow_content_was['nodes']
-    old_node_ids = old_nodes.map { |n| n['data']['id'] }
-    current_nodes = workflow_content['nodes']
-    current_node_ids = current_nodes.map { |n| n['data']['id'] }
+    if workflow_content_changed?
+      old_nodes = workflow_content_was['nodes']
+      old_node_ids = old_nodes.map { |n| n['data']['id'] }
+      current_nodes = workflow_content['nodes']
+      current_node_ids = current_nodes.map { |n| n['data']['id'] }
 
-    added_node_ids = (current_node_ids - old_node_ids)
-    removed_node_ids =  (old_node_ids - current_node_ids)
-    modified_node_ids = (current_nodes - old_nodes).map { |n| n['data']['id'] } - added_node_ids
+      added_node_ids = (current_node_ids - old_node_ids)
+      removed_node_ids =  (old_node_ids - current_node_ids)
+      modified_node_ids = (current_nodes - old_nodes).map { |n| n['data']['id'] } - added_node_ids
 
-    # Resolve the actual nodes from the IDs
-    added_nodes = added_node_ids.map { |i| workflow_content['nodes'].detect { |n| n['data']['id'] == i } }
-    removed_nodes = removed_node_ids.map { |i| workflow_content_was['nodes'].detect { |n| n['data']['id'] == i } }
-    modified_nodes = modified_node_ids.map { |i| workflow_content['nodes'].detect { |n| n['data']['id'] == i } }
+      # Resolve the actual nodes from the IDs
+      added_nodes = added_node_ids.map { |i| workflow_content['nodes'].detect { |n| n['data']['id'] == i } }
+      removed_nodes = removed_node_ids.map { |i| workflow_content_was['nodes'].detect { |n| n['data']['id'] == i } }
+      modified_nodes = modified_node_ids.map { |i| workflow_content['nodes'].detect { |n| n['data']['id'] == i } }
 
-    if added_node_ids.any? || removed_node_ids.any? || modified_node_ids.any?
-      self.create_activity :modify_diagram, parameters: {
-          added_nodes: added_nodes,
-          removed_nodes: removed_nodes,
-          modified_nodes: modified_nodes
-      }
+      if added_node_ids.any? || removed_node_ids.any? || modified_node_ids.any?
+        self.create_activity :modify_diagram, parameters: {
+            added_nodes: added_nodes,
+            removed_nodes: removed_nodes,
+            modified_nodes: modified_nodes
+        }
+      end
     end
   end
 
