5d2a389671320fab0d9f584d5eabd69934aec06b
Niall Beard <niallbeard@gmail.com>
2016-09-19 17:54:37 +0100

    Added icon for scraped today and scraped a while ago. Only can be seen by admins

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -6,31 +6,4 @@ module EventsHelper
 
       "If your website contains training events that you wish to include in TeSS, please contact the TeSS team (<a href='mailto:tess@elixir-uk.info'>tess@elixir-uk.info</a>) for further details."
 
-  ICONS = {
-      started: {:icon => 'fa-hourglass-half', :message => 'This event has already started'},
-      expired: {:icon => 'fa-hourglass-end', :message => 'This event has finished'},
-      online: {:icon => 'fa-desktop', :message => 'This is an online event'},
-      face_to_face: {:icon => 'fa-users', :message => 'This is a physical event'},
-      for_profit: {:icon => 'fa-credit-card', :message => 'This event is from a for-profit company'}
-  }
-
-  def icon_for(type, size=nil)
-
-    return "<i class=\"fa #{ICONS[type][:icon]} has-tooltip event-info-icon#{'-' + size.to_s if size}\"
-    aria-hidden=\"true\"
-    data-toggle=\"tooltip\"
-    data-placement=\"top\"
-    title=\"#{ICONS[type][:message]}\">
-    </i>".html_safe
-  end
-
-  def started_event
-
-  end
-
-  def expired_event
-
-  end
-
-
 end
--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -11,13 +11,20 @@ class ApplicationHelperTest < ActionView::TestCase
     @new_material.scraper_record = true
   end
 
-  test "outdated image supplied for old material" do
-    result = conditional_icon_for(:not_scraped_recently,@old_material)
-    assert_not_equal(conditional_icon_for(:not_scraped_recently,@old_material),nil)
+
+  test "icon should be correct for material scraped today" do
+    expected_result  = "<span class='fresh-icon pull-right'>#{icon_for(:scraped_today, 'large')}</span>".html_safe
+    assert_equal(scrape_status_icon(@new_material, 'large'),expected_result)
+    assert_match /fresh-icon/, expected_result
+    assert_match /fa-check-circle-o/, expected_result
   end
 
-  test "no outdated image supplied for new material" do
-    assert_equal(conditional_icon_for(:not_scraped_recently,@new_material),nil)
+  test "icon should be correct for material not scraped for a while" do
+    expected_result = "<span class='stale-icon pull-right'>#{icon_for(:not_scraped_recently, 'large')}</span>".html_safe
+    expected_result.gsub!(/%SUB%/, (@old_material.last_scraped.to_s))
+    assert_equal(scrape_status_icon(@old_material, 'large'),expected_result)
+    assert_match /stale-icon/, expected_result
+    assert_match /fa-exclamation-circle/, expected_result
   end
 
 end
