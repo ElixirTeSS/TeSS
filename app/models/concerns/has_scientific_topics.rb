9bfef2c988f70c7dbd91bc7b4c8d180b795bdfa9
Niall Beard <niallbeard@gmail.com>
2016-09-29 13:23:08 +0100

    Check name is not nil or blank before finding topics

--- app/models/concerns/has_scientific_topics.rb
+++ app/models/concerns/has_scientific_topics.rb
@@ -9,13 +9,14 @@ module HasScientificTopics
 
   def scientific_topic_names= names
     topics = []
-
     [names].flatten.each do |name|
+      unless name.blank? or name == ''
+        st = ScientificTopic.where(preferred_label: name).to_a
+        st = ScientificTopic.where("'#{name}' = ANY (has_exact_synonym)").to_a if st.empty?
+        st = ScientificTopic.where("'#{name}' = ANY (has_narrow_synonym)").to_a if st.empty?
+        topics << st
+      end
 
-      st = ScientificTopic.where(preferred_label: name).to_a
-      st = ScientificTopic.where("'#{name}' = ANY (has_exact_synonym)").to_a if st.empty?
-      st = ScientificTopic.where("'#{name}' = ANY (has_narrow_synonym)").to_a if st.empty?
-      topics << st
     end
     self.scientific_topics = topics.flatten
   end
