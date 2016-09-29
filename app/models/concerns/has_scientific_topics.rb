ed45ed44f14f79fc2c512185500067c46d04a2eb
Finn Bacall <finn.bacall@manchester.ac.uk>
2016-09-29 13:13:09 +0100

    Stopped rogue scientific topic being added to new events. Fixes #345

--- app/models/concerns/has_scientific_topics.rb
+++ app/models/concerns/has_scientific_topics.rb
@@ -11,13 +11,13 @@ module HasScientificTopics
     topics = []
 
     [names].flatten.each do |name|
-
       st = ScientificTopic.where(preferred_label: name).to_a
       st = ScientificTopic.where("'#{name}' = ANY (has_exact_synonym)").to_a if st.empty?
       st = ScientificTopic.where("'#{name}' = ANY (has_narrow_synonym)").to_a if st.empty?
       topics << st
     end
-    self.scientific_topics = topics.flatten
+
+    self.scientific_topics = topics.flatten.uniq
   end
 
   def scientific_topic_names
