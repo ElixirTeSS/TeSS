fbc61d7a50ac734ef842cf7dfda5873c6ed63326
Finn Bacall <finn.bacall@manchester.ac.uk>
2016-10-06 16:06:40 +0100

    Don't attempt to sanitize descriptions if they weren't provided. #349

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -153,8 +153,8 @@ class MaterialsController < ApplicationController
                                                   :licence, :difficulty_level, {:contributors => []},
                                                   {:authors => []}, {:target_audience => []}, {:node_ids => []},
                                                   external_resources_attributes: [:id, :url, :title, :_destroy])
-    mat_params[:short_description] = ActionView::Base.full_sanitizer.sanitize(mat_params[:short_description])
-    mat_params[:long_description] = ActionView::Base.full_sanitizer.sanitize(mat_params[:long_description])
+    mat_params[:short_description] = ActionView::Base.full_sanitizer.sanitize(mat_params[:short_description]) if mat_params.key?(:short_description)
+    mat_params[:long_description] = ActionView::Base.full_sanitizer.sanitize(mat_params[:long_description]) if mat_params.key?(:long_description)
 
     mat_params
   end
