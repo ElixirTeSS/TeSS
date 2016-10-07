a57bb267eba951a914a88916e5e5d9c6f454a353
Finn Bacall <finn.bacall@manchester.ac.uk>
2016-10-07 12:09:16 +0100

    Ensure events and materials have the same policy

--- app/policies/event_policy.rb
+++ app/policies/event_policy.rb
@@ -1,33 +1,3 @@
-class EventPolicy < ApplicationPolicy
+class EventPolicy < ResourcePolicy
 
-  def update?
-    # Admin role can update/destroy any object, other roles can only update objects they created
-    return true if @user.is_admin? # allow admin roles for all requests - UI and API
-    if request_is_api?(@request) #is this an API action - allow api_user roles only
-      if @user.has_role?(:api_user) or @user.is_owner?(@record) # check ownership
-        return true
-      else
-        return false
-      end
-    end
-    if @user.has_role?(:curator) or @user.is_owner?(@record)
-      return true
-    else
-      return false
-    end
-  end
-
-  def edit?
-    update?
-  end
-
-  def destroy?
-    update?
-  end
-
-  class Scope < Scope
-    def resolve
-      Event.all
-    end
-  end
 end
---
+++ app/policies/resource_policy.rb
@@ -0,0 +1,34 @@
+# A "super" policy for Events and Materials
+
+class ResourcePolicy < ApplicationPolicy
+  def update?
+    # Admin role can update/destroy any object, other roles can only update objects they created
+    return true if @user.is_admin? # allow admin roles for all requests - UI and API
+    if request_is_api?(@request) #is this an API action - allow api_user roles only
+      if @user.has_role?(:api_user) or @user.is_owner?(@record) # check ownership
+        return true
+      else
+        return false
+      end
+    end
+    if @user.has_role?(:curator) or @user.is_owner?(@record)
+      return true
+    else
+      return false
+    end
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    update?
+  end
+
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+end
