7bd8a56b77fda1841c0c27bd66b480e29106d4c6
Finn Bacall <finn.bacall@manchester.ac.uk>
2016-10-14 12:03:39 +0100

    Fixed collaboration box not working

--- app/assets/javascripts/collaborations.js
+++ app/assets/javascripts/collaborations.js
@@ -3,16 +3,17 @@ var Collaborations = {
     fetch: function () {
         var url = $('#collaborators-modal').data('url');
         $.ajax({
-            url: url
-        }).success(function(data) {
-            $('.collaboration-list').html('');
-            data.forEach(function (collaboration) {
-                $('.collaboration-list').append(HandlebarsTemplates['collaborations/collaboration'](collaboration));
-            });
-            Collaborations.displayEmptyText();
+            url: url,
+            success : function(data) {
+                $('.collaboration-list').html('');
+                data.forEach(function (collaboration) {
+                    $('.collaboration-list').append(HandlebarsTemplates['collaborations/collaboration'](collaboration));
+                });
+                Collaborations.displayEmptyText();
+            }
         });
     },
-    
+
     delete: function () {
         var url = $('#collaborators-modal').data('url');
         var element = $(this).parent('li');
@@ -20,10 +21,11 @@ var Collaborations = {
         $.ajax({
             url: url + '/' + element.data('id'),
             dataType: 'json',
-            method: 'DELETE'
-        }).success(function() {
-            element.remove();
-            Collaborations.displayEmptyText();
+            method: 'DELETE',
+            success: function() {
+                element.remove();
+                Collaborations.displayEmptyText();
+            }
         });
     },
 
@@ -33,13 +35,14 @@ var Collaborations = {
             $.ajax({
                 url: url,
                 method: 'POST',
-                data: { collaboration: { user_id: $('#collaborators-modal-add-id').val() } }
-            }).success(function(collaboration) {
-                $('#collaborators-modal-add').val('');
-                $('#collaborators-modal-add-btn').addClass('disabled');
-                var element = $(HandlebarsTemplates['collaborations/collaboration'](collaboration)).addClass('new');
-                $('.collaboration-list').append(element);
-                Collaborations.displayEmptyText();
+                data: { collaboration: { user_id: $('#collaborators-modal-add-id').val() } },
+                success: function(collaboration) {
+                    $('#collaborators-modal-add').val('');
+                    $('#collaborators-modal-add-btn').addClass('disabled');
+                    var element = $(HandlebarsTemplates['collaborations/collaboration'](collaboration)).addClass('new');
+                    $('.collaboration-list').append(element);
+                    Collaborations.displayEmptyText();
+                }
             });
         }
     },
