4969a3d7500b2a4e251827f688d8b96b74603b97
Finn Bacall <finn.bacall@manchester.ac.uk>
2016-11-09 11:28:06 +0000

    Removed unused method

--- app/helpers/workflows_helper.rb
+++ app/helpers/workflows_helper.rb
@@ -3,16 +3,4 @@ module WorkflowsHelper
   WORKFLOWS_INFO = "Training workflows in TeSS are visual, step-by-step protocols that allow users to navigate materials in TeSS in topic- or task-related ways.\n\n"+
       "For example, they may encapsulate typical stages of particular kinds of data analysis (protein sequence analysis, structure analysis, etc.), where each stage/node in the workflow represents a given analysis step and links to the corresponding training resources."
 
-  # Convert Markdown descriptions of workflow nodes/edges to HTML and insert them into elements' data (passed in as JSON)
-  def convert_markdown_descriptions_into_html(json)
-    return "null" if json.blank?
-    workflow = JSON.parse(json)
-    wf_elements_to_change = (workflow['nodes'] || []) + (workflow['edges'] || [])
-    wf_elements_to_change.each do |element|
-      element['data']['html_description'] = render_markdown(element['data']['description'])
-    end
-    return workflow.to_json
-  end
-
-
 end
