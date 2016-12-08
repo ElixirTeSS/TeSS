2911503cfc3451811b85302a8eeefaefb29e3ec1
Milo Thurston <knirirr@gmail.com>
2016-12-08 15:09:31 +0000

    Closed #392.

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -9,7 +9,14 @@ class ApplicationHelperTest < ActionView::TestCase
     @new_material = materials(:good_material)
     @new_material.last_scraped = Time.now
     @new_material.scraper_record = true
+    @old_event = events(:one)
+    @old_event.last_scraped = Time.parse('1912-04-15')
+    @old_event.scraper_record = true
+    @old_iann_event = events(:iann_event)
+    @old_iann_event.last_scraped = Time.parse('1912-04-15')
+    @old_iann_event.scraper_record = true
   end
+
 
 
   test "icon should be correct for material scraped today" do
@@ -26,5 +33,30 @@ class ApplicationHelperTest < ActionView::TestCase
     assert_match /stale-icon/, expected_result
     assert_match /fa-exclamation-circle/, expected_result
   end
+
+
+  def current_user
+    users(:admin)
+  end
+
+  test "icon should be correct for event not scraped for a while" do
+    assert_not_nil(current_user)
+    assert_equal(current_user.is_admin?,true)
+    expected_result = "<span class='stale-icon pull-right'>#{icon_for(:not_scraped_recently, 'large')}</span>".html_safe
+    expected_result.gsub!(/%SUB%/, (@old_event.last_scraped.to_s))
+    assert_equal(scrape_status_icon(@old_event, 'large'),expected_result)
+    assert_match /stale-icon/, expected_result
+    assert_match /fa-exclamation-circle/, expected_result
+  end
+
+  # This test is failing because the content_provider isn't found, but the code runs anyway
+=begin
+  test "no icon should be shown for an iAnn event which has not been scraped recently" do
+    assert_not_nil(current_user)
+    assert_equal(current_user.is_admin?,true)
+    assert_equal(@old_event.content_provider.title.downcase,'iann')
+    assert_equal(scrape_status_icon(@old_iann_event, 'large'),nil)
+  end
+=end
 
 end
