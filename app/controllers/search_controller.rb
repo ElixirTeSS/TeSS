2266e79ed6db0c742ff33b8eeadcbdcce882ec23
Finn Bacall <finn.bacall@manchester.ac.uk>
2017-02-22 10:07:48 +0000

    Don't attempt to search if search is not enabled

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,10 +8,12 @@ class SearchController < ApplicationController
   def index
     @results = {}
 
-    SEARCH_MODELS.each do |model_name|
-      @results[model_name.underscore.pluralize.to_sym] = Sunspot.search(model_name.constantize) do
-        fulltext search_params
-        with('end').greater_than(Time.zone.now) if model_name == 'Event'
+    if TeSS::Config.solr_enabled
+      SEARCH_MODELS.each do |model_name|
+        @results[model_name.underscore.pluralize.to_sym] = Sunspot.search(model_name.constantize) do
+          fulltext search_params
+          with('end').greater_than(Time.zone.now) if model_name == 'Event'
+        end
       end
     end
 
