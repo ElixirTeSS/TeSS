22b329a85588bbb9d7fbd83e98ebfaeb7ae4fb96
Finn Bacall <finn.bacall@manchester.ac.uk>
2017-03-09 17:30:11 +0000

    Store the URI for the ontology term!

--- lib/ontologies/ontology_term.rb
+++ lib/ontologies/ontology_term.rb
@@ -1,10 +1,11 @@
 class OntologyTerm
 
-  attr_reader :ontology, :data
+  attr_reader :ontology, :data, :uri
 
   def initialize(ontology, statements)
     @ontology = ontology
     @data = {}
+    @uri = statements.first.subject.to_s
 
     statements.each do |statement|
       @data[statement.predicate] ||= []
