60d6cccd64b3cf639d14d8bf1b8d517b16aa86b5
Finn Bacall <finn.bacall@manchester.ac.uk>
2017-03-28 16:23:36 +0100

    Convenience methods for getting synonyms of an EDAM term + make inspect less busy

--- lib/ontologies/edam.rb
+++ lib/ontologies/edam.rb
@@ -9,7 +9,23 @@ module EDAM
     end
 
     def synonyms
+      has_exact_synonym
+    end
+
+    def has_exact_synonym
       data[OBO.hasExactSynonym] ? data[OBO.hasExactSynonym].map(&:value) : []
+    end
+
+    def has_narrow_synonym
+      data[OBO.hasNarrowSynonym] ? data[OBO.hasNarrowSynonym].map(&:value) : []
+    end
+
+    def has_broad_synonym
+      data[OBO.hasBroadSynonym] ? data[OBO.hasBroadSynonym].map(&:value) : []
+    end
+
+    def inspect
+      "<#{self.class} @uri=#{self.uri}, preferred_label: #{self.preferred_label}>"
     end
 
   end
