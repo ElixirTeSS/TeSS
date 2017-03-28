930fa7d915f3ce3511a51d11730df93c3aa4af52
Finn Bacall <finn.bacall@manchester.ac.uk>
2017-03-28 16:22:35 +0100

    Fixed various topic assignment bugs

--- app/models/concerns/has_scientific_topics.rb
+++ app/models/concerns/has_scientific_topics.rb
@@ -7,29 +7,28 @@ module HasScientificTopics
   end
 
   def scientific_topic_names= names
-    topics = []
+    terms = []
     [names].flatten.each do |name|
       unless name.blank? or name == ''
-        st = []
-        st << EDAM::Ontology.instance.lookup_by_name(name)
+        st = [EDAM::Ontology.instance.lookup_by_name(name)].compact
         st = EDAM::Ontology.instance.find_by(OBO.hasExactSynonym, name) if st.empty?
         st = EDAM::Ontology.instance.find_by(OBO.hasNarrowSynonym, name) if st.empty?
-        topics << st
+        terms += st
       end
     end
-    self.scientific_topics = topics.flatten.uniq
+    self.scientific_topics = terms.uniq
   end
 
   def scientific_topic_names
     scientific_topics.map(&:preferred_label)
   end
 
-  def scientific_topics= uris
-    uris.each { |uri| scientific_topic_links.build(term_uri: uri) }
+  def scientific_topics= terms
+    terms.each { |term| scientific_topic_links.build(term_uri: term.uri) if term && term.uri }
   end
 
   def scientific_topics
-    scientific_topic_links.map(&:scientific_topics)
+    scientific_topic_links.map(&:scientific_topic)
   end
 
 end
