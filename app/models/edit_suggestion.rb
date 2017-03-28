4088f675d1ad4cfffc0094bf5707e1219396d674
Finn Bacall <finn.bacall@manchester.ac.uk>
2017-03-28 16:24:12 +0100

    Make EditSuggestions use HasScientificTopics concern to simplify ScientificTopic refactoring

--- app/models/edit_suggestion.rb
+++ app/models/edit_suggestion.rb
@@ -1,4 +1,5 @@
 class EditSuggestion < ActiveRecord::Base
   belongs_to :suggestible, polymorphic: true
-  has_and_belongs_to_many :scientific_topics, dependent: :nullify
+
+  include HasScientificTopics
 end
