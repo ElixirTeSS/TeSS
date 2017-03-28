8c1912a1eab5f49854aea73b2d2d8b65c05f4b15
Finn Bacall <finn.bacall@manchester.ac.uk>
2017-03-28 16:53:50 +0100

    Test fixes

--- test/workers/edit_suggestion_worker_test.rb
+++ test/workers/edit_suggestion_worker_test.rb
@@ -22,7 +22,7 @@ class EditSuggestionWorkerTest < ActiveSupport::TestCase
 
     assert_not_nil material.edit_suggestion
     assert_equal 2, material.edit_suggestion.scientific_topics.count
-    assert_equal 'RNA', material.edit_suggestion.scientific_topics.first.preferred_label
+    assert_equal ['Small molecules', 'Molecular dynamics'], material.edit_suggestion.scientific_topics.map(&:preferred_label)
   end
 
   test 'Get suggestions for an event' do
@@ -40,7 +40,7 @@ class EditSuggestionWorkerTest < ActiveSupport::TestCase
 
     assert_not_nil event.edit_suggestion
     assert_equal 2, event.edit_suggestion.scientific_topics.count
-    assert_equal 'RNA', event.edit_suggestion.scientific_topics.first.preferred_label
+    assert_equal ['Small molecules', 'Molecular dynamics'], event.edit_suggestion.scientific_topics.map(&:preferred_label)
   end
 
   test 'Get suggestions for a workflow' do
@@ -58,7 +58,7 @@ class EditSuggestionWorkerTest < ActiveSupport::TestCase
 
     assert_not_nil workflow.edit_suggestion
     assert_equal 2, workflow.edit_suggestion.scientific_topics.count
-    assert_equal 'RNA', workflow.edit_suggestion.scientific_topics.first.preferred_label
+    assert_equal ['Small molecules', 'Molecular dynamics'], workflow.edit_suggestion.scientific_topics.map(&:preferred_label)
   end
 
 end
