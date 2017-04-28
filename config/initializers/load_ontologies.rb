2557c043f7c37bc909b16aa26218c8290eeae450
Finn Bacall <finn.bacall@manchester.ac.uk>
2017-04-28 14:33:41 +0100

    Don't load ontology every time the app changes in development mode

--- config/initializers/load_ontologies.rb
+++ config/initializers/load_ontologies.rb
@@ -1,3 +1,5 @@
+require 'ontologies/edam/ontology'
+
 OBO = RDF::Vocabulary.new('http://www.geneontology.org/formats/oboInOwl#')
 OBO_EDAM = RDF::Vocabulary.new('http://purl.obolibrary.org/obo/edam#')
 
