0e85ed23e3638236705d9d961a576b618599fe44
Finn Bacall <finn.bacall@manchester.ac.uk>
2017-07-20 09:24:46 +0100

    Simplifying event and material forms. #472

---
+++ app/inputs/datetime_picker_input.rb
@@ -0,0 +1,7 @@
+class DatetimePickerInput < SimpleForm::Inputs::Base
+  def input(wrapper_options)
+    merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
+
+    template.datetime_picker(@builder, attribute_name)
+  end
+end
