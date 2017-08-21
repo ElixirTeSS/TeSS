780847aa2764e3d063f6699f041891016b706b15
Finn Bacall <finn.bacall@manchester.ac.uk>
2017-08-21 16:07:43 +0100

    Re-worded content provider info text

--- app/helpers/content_providers_helper.rb
+++ app/helpers/content_providers_helper.rb
@@ -1,4 +1,4 @@
 module ContentProvidersHelper
 
-  CONTENT_PROVIDERS_INFO = "Content providers are entities – e.g. institutions members of ELIXIR nodes, independent institutions or related portals, like GOBLET, the Software and Data Carpentry Foundations, etc., or individual people – that provide training materials of relevance to life sciences and ELIXIR."
+  CONTENT_PROVIDERS_INFO = "Content providers are entities (such as academic institutions, non-profit organisations, portals etc.) that provide training materials of relevance to life sciences and ELIXIR."
 end
