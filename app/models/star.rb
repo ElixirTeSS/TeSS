4a3b5813d3f718c916bcb80b3d8411ba7ea44b49
Finn Bacall <finn.bacall@manchester.ac.uk>
2017-09-05 13:58:47 +0100

    Validations and tests for stars. Fixes #474

--- app/models/star.rb
+++ app/models/star.rb
@@ -3,6 +3,17 @@ class Star < ActiveRecord::Base
   belongs_to :user
   belongs_to :resource, polymorphic: true
 
-  validates :resource, presence: true
+  validates :resource_id, presence: true, uniqueness: { scope: [:resource_type, :user_id] }
+  validate :check_resource_exists
 
+  private
+
+  def check_resource_exists
+    begin
+      resource_class = resource_type.constantize
+      errors.add(:resource, 'is not valid') unless resource_class.exists?(resource_id)
+    rescue NameError
+      errors.add(:resource_type, 'is not valid')
+    end
+  end
 end
---
+++ test/controllers/stars_controller_test.rb
@@ -0,0 +1,78 @@
+require 'test_helper'
+
+class StarsControllerTest < ActionController::TestCase
+
+  include Devise::Test::ControllerHelpers
+
+  test "can star a resource" do
+    sign_in users(:regular_user)
+    material = materials(:good_material)
+
+    assert_difference('Star.count') do
+      post :create, star: { resource_id: material.id, resource_type: material.class.name }, format: :json
+    end
+
+    assert_response :success
+  end
+
+  test "does not add duplicate star to resource" do
+    user = users(:regular_user)
+    sign_in user
+    material = materials(:good_material)
+    user.stars.create(resource: material)
+
+    assert_no_difference('Star.count') do
+      post :create, star: { resource_id: material.id, resource_type: material.class.name }, format: :json
+    end
+
+    assert_response :success
+  end
+
+  test "can un-star resource" do
+    user = users(:regular_user)
+    sign_in user
+    material = materials(:good_material)
+    user.stars.create(resource: material)
+
+    assert_difference('Star.count', -1) do
+      delete :destroy, star: { resource_id: material.id, resource_type: material.class.name }, format: :json
+    end
+
+    assert_response :success
+  end
+
+  test "cannot star a resource if not logged in" do
+    material = materials(:good_material)
+
+    assert_no_difference('Star.count') do
+      post :create, star: { resource_id: material.id, resource_type: material.class.name }, format: :json
+    end
+
+    assert_response :unauthorized
+  end
+
+  test "cannot create bad star" do
+    user = users(:regular_user)
+    sign_in user
+    material = materials(:good_material)
+
+    assert_no_difference('Star.count') do
+      post :create, star: { resource_id: material.id, resource_type: 'Benjamin' }, format: :json
+    end
+
+    assert_response :unprocessable_entity
+  end
+
+  test "can view list of stars" do
+    user = users(:regular_user)
+    sign_in user
+    material = materials(:good_material)
+    user.stars.create(resource: material)
+
+    get :index
+
+    assert_response :success
+    assert_select '#materials div.search-results-count', text: "1 Material", count: 1
+    assert_select '#materials a.btn-block[href=?]', material_path(material), text: material.title, count: 1
+  end
+end
