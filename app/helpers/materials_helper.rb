4232e837e1378bf6b7f3fa3839ab9cc5961d7be3
Milo Thurston <knirirr@gmail.com>
2017-11-03 15:01:22 +0000

    Replaced hard-coded email addresses with TeSS::Config value.

--- app/helpers/materials_helper.rb
+++ app/helpers/materials_helper.rb
@@ -1,7 +1,7 @@
 module MaterialsHelper
   MATERIALS_INFO = "In the context of TeSS, a training material is a link to a single online training material sourced by a content provider (such as a text on a Web page, presentation, video, etc.) along with description and other meta information (e.g. ontological categorization, keywords, etc.).\n\n"\
   "TeSS harvests materials automatically, including descriptions and other relevant meta-data made available by providers. Materials can also be registered manually.\n\n"\
-  "If your website contains training materials that you wish to include in TeSS, please contact the TeSS team (<a href='mailto:tess @elixir-uk.info'>tess@elixir-uk.info</a>) for further details.".freeze
+  "If your website contains training materials that you wish to include in TeSS, please contact the TeSS team (<a href='#{TeSS::Config.contact_email}'>#{TeSS::Config.contact_email}</a>) for further details.".freeze
 
   TOPICS_INFO = "TeSS generates a scientific topic suggestion for each resource registered. It does this by
   passing the description and title of the resource to the Bioportal Annotator Web service.
