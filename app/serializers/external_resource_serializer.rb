07dc6b0f6fd6e4adb681d88e25a4a60855a41322
Finn Bacall <finn.bacall@manchester.ac.uk>
2017-11-15 11:45:49 +0000

    Not using this anymore

--- app/serializers/external_resource_serializer.rb
+++
@@ -1,7 +0,0 @@
-class ExternalResourceSerializer < ApplicationSerializer
-
-  attributes :title, :url, :created_at, :updated_at
-  attribute(:api_url) { object.api_url_of_tool }
-  attribute(:type) { object.is_tool? ? 'tool' : 'other' }
-
-end
