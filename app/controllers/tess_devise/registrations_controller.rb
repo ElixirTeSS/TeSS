7600d4e8b43324dc32ae621e37562097bb274649
Finn Bacall <finn.bacall@manchester.ac.uk>
2018-02-13 13:27:18 +0000

    Added breadcrumbs to subscriptions and manage account pages

--- app/controllers/tess_devise/registrations_controller.rb
+++ app/controllers/tess_devise/registrations_controller.rb
@@ -1,6 +1,7 @@
 class TessDevise::RegistrationsController < Devise::RegistrationsController
   # Inspired by http://stackoverflow.com/questions/3546289/override-devise-registrations-controller
   before_action :check_captcha, only: :create
+  before_action :set_breadcrumbs, only: :edit
 
   # Set the after update path to be user's show page
   # instead the default root_path
@@ -16,5 +17,10 @@ class TessDevise::RegistrationsController < Devise::RegistrationsController
       self.resource = resource_class.new sign_up_params
       respond_with_navigational(resource) { render :new }
     end
+  end
+
+  def set_breadcrumbs
+    add_base_breadcrumbs('users')
+    @breadcrumbs += [{ name: @user.name, url: user_path(@user) }, { name: 'Manage Account' }]
   end
 end
