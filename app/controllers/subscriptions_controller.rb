aacaf4e6802af35be1aa72efa04e93a46d1c3aed
Finn Bacall <finn.bacall@manchester.ac.uk>
2018-02-14 10:17:33 +0000

    Only needs breadcrumbs on index page

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -3,7 +3,7 @@ class SubscriptionsController < ApplicationController
   skip_before_action :authenticate_user!, :authenticate_user_from_token!, only: :unsubscribe
   before_action :authenticate_user!, only: :index
   before_filter :find_subscription, only: [:destroy, :unsubscribe]
-  before_action :set_breadcrumbs
+  before_action :set_breadcrumbs, only: :index
 
   def index
     @subscriptions = current_user.subscriptions.order('created_at DESC')
