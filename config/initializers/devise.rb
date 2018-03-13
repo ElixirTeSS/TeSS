1f8ddc2fb750c5e5371508733e60db77ba2e2823
Finn Bacall <finn.bacall@manchester.ac.uk>
2018-03-13 09:38:24 +0000

    Made SMTP configurable in production mode. Made mailer sender address configurable

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'tess-registrations@elixir.org.uk'
+  config.mailer_sender = TeSS::Config.sender_email
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
