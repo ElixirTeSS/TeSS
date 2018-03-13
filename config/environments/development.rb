c963e3c5a273c6e5335d16fb051b4dd00248ecb5
Finn Bacall <finn.bacall@manchester.ac.uk>
2018-03-13 09:45:41 +0000

    Use default SMTP settings if no SMTP block in config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,5 +44,5 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = Rails.application.secrets[:smtp]
+  config.action_mailer.smtp_settings = Rails.application.secrets[:smtp] if Rails.application.secrets.key?(:smtp)
 end
