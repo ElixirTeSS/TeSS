c574dac7c1f3bd351b6886464ef523758d6f6781
Finn Bacall <finn.bacall@manchester.ac.uk>
2018-08-02 10:26:37 +0100

    Regenerated various configs (via `rails app:update`)

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
 require 'rails/all'
 
@@ -12,24 +12,9 @@ module TeSS
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-
     #Include lib/** files (lib directory and all subdirectories)
     config.autoload_paths += Dir["#{Rails.root}/lib/**/"]
 
-    # Route exceptions to the application router vs. default
-    config.exceptions_app = self.routes
-
-    # Enable CORS
     config.middleware.insert_before 0, 'Rack::Cors' do
       allow do
         origins '*'
