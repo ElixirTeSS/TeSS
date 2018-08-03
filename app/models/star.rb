ccafe025da35dc7abb1bb6c7ecb6646eedbd43b3
Finn Bacall <finn.bacall@manchester.ac.uk>
2018-08-03 09:40:23 +0100

    `belongs_to` are validated by default in Rails 5

--- app/models/star.rb
+++ app/models/star.rb
@@ -4,16 +4,5 @@ class Star < ApplicationRecord
   belongs_to :resource, polymorphic: true
 
   validates :resource_id, presence: true, uniqueness: { scope: [:resource_type, :user_id] }
-  validate :check_resource_exists
 
-  private
-
-  def check_resource_exists
-    begin
-      resource_class = resource_type.constantize
-      errors.add(:resource, 'is not valid') unless resource_class.exists?(resource_id)
-    rescue NameError
-      errors.add(:resource_type, 'is not valid')
-    end
-  end
 end
