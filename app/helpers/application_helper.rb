cd6c700a3797d560c1099363e4ba0d30f466e1ae
Finn Bacall <finn.bacall@manchester.ac.uk>
2018-09-21 09:44:35 +0100

    Make links in descriptions clickable. Fixes #556

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -105,7 +105,7 @@ module ApplicationHelper
 
   def render_markdown(markdown_text, options = {}, renderer_options = {})
     if markdown_text
-      options.reverse_merge!(filter_html: true, tables: true)
+      options.reverse_merge!(filter_html: true, tables: true, autolink: true)
       renderer_options.reverse_merge!(hard_wrap: true, link_attributes: { target: '_blank' })
       Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(renderer_options), options).render(markdown_text).html_safe
     else
