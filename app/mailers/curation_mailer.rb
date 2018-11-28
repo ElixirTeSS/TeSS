c0e9c92bd5f0cf0b5b3ea89ba73970ed57036b4d
Finn Bacall <finn.bacall@manchester.ac.uk>
2018-11-28 10:06:43 +0000

    Attempt to fix issue where list of created resources is empty in user approval email

--- app/mailers/curation_mailer.rb
+++ app/mailers/curation_mailer.rb
@@ -5,7 +5,7 @@ class CurationMailer < ApplicationMailer
 
   def user_requires_approval(user)
     @user = user
-    @resources = user.created_resources
+    @resources = user.reload.created_resources
     subject = "TeSS user \"#{user.name}\" requires approval"
     mail(subject: subject, to: USER_APPROVAL_RECIPIENTS.map(&:email)) do |format|
       format.html
