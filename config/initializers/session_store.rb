d8914c0d61af7c4ea9bde29dcb393f15c987ef9f
Finn <finn.bacall@manchester.ac.uk>
2019-08-15 10:46:35 +0100

    Another attempt to secure cookies

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
 #Rails.application.config.session_store :cookie_store, key: '_tess_sessions'
-Rails.application.config.session_store :active_record_store, key: '_tess_sessions'
+Rails.application.config.session_store :active_record_store, key: '_tess_sessions', secure: Rails.env.production?
 
 
