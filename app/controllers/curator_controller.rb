9cd2fe5e4a087910034931fb1135d33fae0018d1
Finn Bacall <finn.bacall@manchester.ac.uk>
2020-06-19 11:53:35 +0100

    Faster way of finding users with content (for curation)
    Paginated user curation page

--- app/controllers/curator_controller.rb
+++ app/controllers/curator_controller.rb
@@ -37,8 +37,10 @@ class CuratorController < ApplicationController
     @role ||= Role.fetch('unverified_user')
     @users = User.with_role(@role).order('created_at DESC')
     if params[:with_content]
-      @users = @users.to_a.select { |u| u.created_resources.any? }
+      @users = @users.includes(:events, :materials, :workflows, :content_providers).with_created_resources
     end
+
+    @users = @users.paginate(page: params[:page], per_page: params[:per_page] || 100)
 
     respond_to do |format|
       format.html
