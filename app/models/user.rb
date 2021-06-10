ad3a5a150d76a4de116d4bb102304f1f58edb7fb
eResEngineer <nmay@walroz.org>
2021-06-10 15:29:32 +1000

    Disable registration.

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,7 +47,7 @@ class User < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
+  devise :database_authenticatable, :confirmable, # :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :authentication_keys => [:login]
 
