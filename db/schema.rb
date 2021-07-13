a4f67cc05f215a80c6c4dabf70f3fd38f8eec99e
eResEngineer <nmay@walroz.org>
2021-07-13 18:53:46 +1000

    Updated schema.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_13_025428) do
+ActiveRecord::Schema.define(version: 2021_07_13_082501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -216,6 +216,7 @@ ActiveRecord::Schema.define(version: 2021_07_13_025428) do
     t.text "syllabus"
     t.string "subsets", default: [], array: true
     t.text "contact"
+    t.text "learning_objectives"
     t.index ["content_provider_id"], name: "index_materials_on_content_provider_id"
     t.index ["slug"], name: "index_materials_on_slug", unique: true
     t.index ["user_id"], name: "index_materials_on_user_id"
