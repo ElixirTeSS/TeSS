4c2b1e5a81f38bdaeb7c5ab0dfe239a7e7d9ee18
eResEngineer <nmay@walroz.org>
2021-07-22 13:23:49 +1000

    Fix erroneous refactor of 'long_description'.

--- db/migrate/20210721074829_rename_column_on_materials.rb
+++ db/migrate/20210721074829_rename_column_on_materials.rb
@@ -1,5 +1,5 @@
 class RenameColumnOnMaterials < ActiveRecord::Migration[5.2]
   def change
-    rename_column :materials, :description, :description
+    rename_column :materials, :long_description, :description
   end
 end
