19ca6d64ee8e8894be686e732b3dccee62d7fe5c
Chris Child <chris.child@gmail.com>
2021-08-05 15:44:48 +0100

    << deprecated: use add

--- app/validators/controlled_vocabulary_validator.rb
+++ app/validators/controlled_vocabulary_validator.rb
@@ -4,11 +4,11 @@ class ControlledVocabularyValidator < ActiveModel::EachValidator
     if value.respond_to?(:each)
       bad_terms = value.reject { |v| options[:dictionary].lookup(v) }
       if bad_terms.any?
-        record.errors[attribute] << (options[:message] || "contained invalid terms: #{bad_terms.join(', ')}")
+        record.errors.add(attribute, (options[:message] || "contained invalid terms: #{bad_terms.join(', ')}"))
       end
     else
       unless options[:dictionary].lookup(value)
-        record.errors[attribute] << (options[:message] || "must be a controlled vocabulary term")
+        record.errors.add(attribute, (options[:message] || "must be a controlled vocabulary term"))
       end
     end
   end
