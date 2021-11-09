613082b0a7ca1897b60627e1dd516fa84d155565
Chris Child <chris.child@gmail.com>
2021-11-09 14:39:44 +0000

    add the creation for admin use to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,4 +15,8 @@ path = File.join(Rails.root, 'config', 'data', 'elixir_nodes.json')
 hash = JSON.parse(File.read(path))
 Node.load_from_hash(hash, verbose: false)
 
+# Admin User
+puts "\nSeeding admin user"
+User.create!(:email=>ENV["ADMIN_EMAIL"],:username=>ENV["ADMIN_USERNAME"],:password=>ENV["ADMIN_PASSWORD"],:role=>Role.find_by_name('admin'),:processing_consent=>1)
+
 puts "Done"
