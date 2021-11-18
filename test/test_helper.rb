271966964e727c833b1abd3634f1ee1ba2f8c0a4
DReSA Support <91715679+dresa-org-au@users.noreply.github.com>
2021-11-19 08:36:27 +1100

    Switch to lcov formatter

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,10 @@
 require 'simplecov'
+require 'simplecov-lcov'
+
+SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
 SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new([
-                            SimpleCov::Formatter::HTMLFormatter, ])
+                            SimpleCov::Formatter::HTMLFormatter,
+                            SimpleCov::Formatter::LcovFormatter, ])
 SimpleCov.start 'rails'
 
 ENV['RAILS_ENV'] ||= 'test'
