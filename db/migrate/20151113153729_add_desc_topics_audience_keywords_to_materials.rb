7e6e00b4638ffcf95ac51703aa1781a1c578c7a6
Finn Bacall <finn.bacall@manchester.ac.uk>
2022-05-25 16:32:16 +0100

    Revert migration change

--- db/migrate/20151113153729_add_desc_topics_audience_keywords_to_materials.rb
+++ db/migrate/20151113153729_add_desc_topics_audience_keywords_to_materials.rb
@@ -1,6 +1,6 @@
 class AddDescTopicsAudienceKeywordsToMaterials < ActiveRecord::Migration[4.2]
   def change
-    add_column :materials, :description, :text
+    add_column :materials, :long_description, :text
     add_column :materials, :target_audience, :string, array: true, default: []
     add_column :materials, :scientific_topic, :string, array: true, default: []
     add_column :materials, :keywords, :string, array: true, default: []
