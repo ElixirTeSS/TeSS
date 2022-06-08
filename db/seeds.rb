4064d421edb5d90235d1342d2406ff772e45b83c
Finn Bacall <finn.bacall@manchester.ac.uk>
2022-06-08 15:45:15 +0100

    Don't create duplicate admin user if seeds re-run
    Don't use `admin` as admin name because its reserved...

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,7 @@ Node.load_from_hash(hash, verbose: false)
 
 # Admin User
 puts "\nSeeding admin user"
-User.create!(:email=>ENV["ADMIN_EMAIL"],:username=>ENV["ADMIN_USERNAME"],:password=>ENV["ADMIN_PASSWORD"],:role=>Role.find_by_name('admin'),:processing_consent=>1)
+u = User.find_or_initialize_by(username: ENV["ADMIN_USERNAME"], role: Role.find_by_name('admin'))
+u.update!(email: ENV["ADMIN_EMAIL"], password: ENV["ADMIN_PASSWORD"], processing_consent: 1) unless u.persisted?
 
 puts "Done"
