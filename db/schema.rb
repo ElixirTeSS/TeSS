84d475342aead9a247903db760b2f3420aa36b99
Finn Bacall <finn.bacall@manchester.ac.uk>
2022-07-04 09:32:50 +0100

    Set default timezone. #703

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_27_141304) do
+ActiveRecord::Schema.define(version: 2022_07_01_132558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -170,7 +170,7 @@ ActiveRecord::Schema.define(version: 2022_05_27_141304) do
     t.string "eligibility", default: [], array: true
     t.text "contact"
     t.string "host_institutions", default: [], array: true
-    t.string "timezone"
+    t.string "timezone", default: "UTC"
     t.string "funding"
     t.integer "attendee_count"
     t.integer "applicant_count"
