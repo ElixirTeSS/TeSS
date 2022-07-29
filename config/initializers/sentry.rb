024961844e3fdebe4cc56ef70c2a1f05e042efd6
Daan van Vugt <daan@vanvugt.me>
2022-07-29 11:57:33 +0200

    uva scraper update and rubocop autofix

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,17 +1,15 @@
 # frozen_string_literal: true
 
-if Rails.env.production? && Rails.application.secrets.sentry_url.present?
-  Sentry.init do |config|
-    config.dsn = TeSS::Config.sentry_url
-    config.breadcrumbs_logger = %i[active_support_logger http_logger]
+Sentry.init do |config|
+  config.dsn = Rails.application.secrets.sentry_url
+  config.breadcrumbs_logger = %i[active_support_logger http_logger]
 
-    # Set tracesSampleRate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production
-    config.traces_sample_rate = 1.0
-    # or
-    config.traces_sampler = lambda do |_context|
-      true
-    end
+  # Set tracesSampleRate to 1.0 to capture 100%
+  # of transactions for performance monitoring.
+  # We recommend adjusting this value in production
+  config.traces_sample_rate = 1.0
+  # or
+  config.traces_sampler = lambda do |_context|
+    true
   end
 end
