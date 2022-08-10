8eb6f0fd1298fa03bacdcd126a3b5af20c6205af
Daan van Vugt <daan@vanvugt.me>
2022-08-10 09:54:41 +0200

    Fuzzy matching on event types and eligibilities
    This will make it easier to scrape events from different sources, with
    eligibility and event type information, without running into validation
    issues.
    
    Use a trigram similarity to the dictionary to perform fuzzy matching.
    Use the dictionary key and the content of a (array) match field to match
    on.
    
    example:
    
    first_come_first_served:
      title: First Come First Served
      match:
        - time
        - first
        - registration order

---
+++ lib/fuzzy_dictionary_match.rb
@@ -0,0 +1,41 @@
+module FuzzyDictionaryMatch
+  def self.included(mod)
+    mod.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    # Remove empty array elements
+    def fuzzy_dictionary_match(fields)
+      cattr_accessor :fields_to_match
+
+      self.fields_to_match ||= {}
+      self.fields_to_match.merge!(fields)
+
+      before_validation :match_fields
+
+      include FuzzyDictionaryMatch::InstanceMethods
+    end
+  end
+
+  module InstanceMethods
+    private
+
+    def match_fields
+      self.class.fields_to_match.each do |field, dictionary|
+        if self[field].instance_of? Array
+          self[field].map! do |n|
+            dictionary.best_match(n)
+          end
+        else
+          self[field] = dictionary.best_match(self[field])
+        end
+      end
+    end
+  end
+end
+
+# in your model:
+#   class SomeModel < ApplicationRecord
+#     include FuzzyDictionaryMatch
+#     fuzzy_dictionary_match(event_types: EventTypeDictionary.instance)
+#   end
