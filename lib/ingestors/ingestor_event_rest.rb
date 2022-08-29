b2faae933a033108dd5240946e323bdc5d3970fb
Daan van Vugt <daan@vanvugt.me>
2022-08-29 16:22:31 +0200

    openscience-maastricht scraper

--- lib/ingestors/ingestor_event_rest.rb
+++ lib/ingestors/ingestor_event_rest.rb
@@ -43,6 +43,9 @@ module Ingestors
         { name: 'NWO',
           url: 'https://www.nwo.nl/',
           process: method(:process_nwo) },
+        { name: 'OSCM',
+          url: 'https://www.openscience-maastricht.nl/',
+          process: method(:process_oscm) },
         { name: 'UvA',
           url: 'https://www.uva.nl/_restapi/list-json',
           process: method(:process_uva) }
@@ -569,6 +572,46 @@ module Ingestors
         end
       end
     end
+
+    def process_oscm(url)
+      # Instead of using the sitemap we use the events page.
+      # The sitemap shows also past events, but the ical link for those does not work, so we can't parse them with the below code.
+      Nokogiri::HTML5(URI.open(url)).css('.eventname > a').each do |link|
+        begin
+          event_url = link.attributes['href']
+          event_page = Nokogiri::HTML5.parse(URI.open(event_url))
+
+          # create new event
+          event = Event.new
+
+          # extract event details from ical
+          ical_event = Icalendar::Event.parse(URI.open("#{event_url}/ical/").set_encoding('utf-8')).first
+          event.title = ical_event.summary
+          event.description = convert_description ical_event.description
+          event.url = ical_event.url
+          # TeSS timezone handling is a bit special.
+          event.start = ical_event.dtstart
+          event.end = ical_event.dtend
+          event.venue = ical_event.try(:venue)
+          event.event_types = ical_event.categories # fair-coffee pre-registration-workshop fair-essentials-workshop fair-for-qualitative-data reproducibilitea
+          # see https://www.openscience-maastricht.nl/wp-sitemap-taxonomies-event-categories-1.xml 
+          event.timezone = 'Europe/Amsterdam' # how to get this from Icalendar Event object?
+          # it's not really needed since dtstart and dtend contain timezone information
+          event.source = 'OSCM'
+          event.online = true
+
+          # add event to events array
+          add_event(event)
+          @ingested += 1
+
+          sleep(1)
+        rescue Exception => e
+          @messages << "Extract event fields failed with: #{e.message} for #{event_url}"
+          Sentry.capture_exception(e)
+        end
+      end
+    end
+
 
     def process_dans(url)
       4.times.each do |i| # always check the first 4 pages, # of pages could be increased if needed
