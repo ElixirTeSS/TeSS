76d892e520a7e45afb016a7cbfff8ec0291c6fd5
Finn Bacall <finn.bacall@manchester.ac.uk>
2022-12-01 19:44:38 +0000

    Fix broken `should not permit internal image URL address` test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -27,6 +27,7 @@ require 'vcr'
 VCR.configure do |config|
   config.cassette_library_dir = 'test/vcr_cassettes'
   config.hook_into :webmock
+  config.allow_http_connections_when_no_cassette = true
 end
 
 class ActiveSupport::TestCase
