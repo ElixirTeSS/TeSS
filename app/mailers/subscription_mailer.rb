50cd5e6b45e7a145e98994aa877c97f3c6b6a970
Daan van Vugt <daan@vanvugt.me>
2023-01-03 17:40:32 +0100

    Include a link to any collections the user might have in digest mails
    This gives a nice workflow for getting reminded there are new entries on
    TeSS to curate.

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -8,6 +8,7 @@ class SubscriptionMailer < ApplicationMailer
     @user = sub.user
     @digest = dig
     @subscription = sub
+    @collections = Collection.left_outer_joins(:collaborators).where(collaborators: { id: @user.id}).or(Collection.where(user_id: @user.id)).distinct
     subs = pluralize(@digest.total_count, "new #{@subscription.subscribable_type.downcase}")
     subject = "#{TeSS::Config.site['title_short']} #{sub.frequency} digest - #{subs} matching your criteria"
     mail(subject: subject, to: sub.user.email) do |format|
