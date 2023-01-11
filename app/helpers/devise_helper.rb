2c3832f136b227b0cc9f72d14233f262321bf071
Finn Bacall <finn.bacall@manchester.ac.uk>
2023-01-11 17:43:54 +0000

    Rename helper method to avoid conflict with deprecated devise method

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,6 +1,6 @@
 # The helper for devise classes
 module DeviseHelper
-  def devise_error_messages!(title = nil)
+  def account_error_messages!(title = nil)
     title ||= I18n.t("errors.messages.not_saved",
                       count: resource.errors.count,
                       resource: resource.class.model_name.human.downcase)
