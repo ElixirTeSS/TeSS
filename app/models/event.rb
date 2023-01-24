669eef91f9babeb50f9e4e6304fe4e4bdc814cff
Finn Bacall <finn.bacall@manchester.ac.uk>
2023-01-24 10:59:30 +0000

    Fix node filter not showing, even if feature enabled

--- app/models/event.rb
+++ app/models/event.rb
@@ -172,7 +172,7 @@ class Event < ApplicationRecord
 
   def self.facet_fields
     field_list = %w[ content_provider keywords scientific_topics operations tools fields online event_types
-                     venue city country organizer sponsors target_audience eligibility user collections ]
+                     venue city country organizer sponsors target_audience eligibility user node collections ]
 
     field_list.delete('operations') if TeSS::Config.feature['disabled'].include? 'operations'
     field_list.delete('scientific_topics') if TeSS::Config.feature['disabled'].include? 'topics'
