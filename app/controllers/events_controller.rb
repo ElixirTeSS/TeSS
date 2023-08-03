634bf975b14d17415cdc7c0ea2e8fdfdfd8e9bc3
Daan van Vugt <dvanvugt@ignitioncomputing.com>
2023-08-03 15:27:22 +0200

    allow specifying max event length for showing in calendar

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -37,7 +37,7 @@ class EventsController < ApplicationController
     set_params
     fetch_resources
     # now customize the list by moving all events longer than 3 days into a separate array
-    @long_events, @events = @events.partition { |e| (e.end - e.start) > 3.days }
+    @long_events, @events = @events.partition { |e| (e.end - e.start) > TeSS::Config.site.fetch(:calendar_event_maxlength, 5).to_i.days }
 
     respond_to do |format|
       format.js
