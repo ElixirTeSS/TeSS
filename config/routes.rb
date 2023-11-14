1a1a52cba19deafe5e0e3f554c657ac1f6ac772a
Finn Bacall <finn.bacall@manchester.ac.uk>
2023-11-14 14:48:53 +0000

    Add collaborators

--- config/routes.rb
+++ config/routes.rb
@@ -116,7 +116,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :learning_paths, concerns: :activities
+  resources :learning_paths, concerns: %i[collaboratable activities]
   resources :learning_path_topics, concerns: %i[collaboratable activities]
 
   get 'elearning_materials' => 'materials#index', defaults: { 'resource_type' => 'e-learning' }
