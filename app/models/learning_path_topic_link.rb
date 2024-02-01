ac9d3884932aa0220bc802dda0d6361872d8f3c3
Finn Bacall <finn.bacall@manchester.ac.uk>
2024-02-01 18:48:07 +0000

    Learning paths controller tests

--- app/models/learning_path_topic_link.rb
+++ app/models/learning_path_topic_link.rb
@@ -3,6 +3,7 @@ class LearningPathTopicLink < ApplicationRecord
   belongs_to :topic, foreign_key: :topic_id, class_name: 'LearningPathTopic'
 
   before_create :set_order
+  after_save :log_activity
 
   validates :topic_id, uniqueness: { scope: %i[learning_path_id], message: 'already included in learning path' }
 
@@ -10,5 +11,10 @@ class LearningPathTopicLink < ApplicationRecord
 
   def set_order
     self.order ||= (learning_path.topic_links.maximum(:order) || 0) + 1
+  end
+
+  def log_activity
+    self.learning_path.create_activity(:add_topic, owner: User.current_user,
+                                       parameters: { topic_id: topic.id, topic_title: topic.title })
   end
 end
