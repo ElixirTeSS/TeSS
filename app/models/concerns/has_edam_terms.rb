992e4100512076173be7d904b5360c00f1dba3d6
Finn Bacall <finn.bacall@manchester.ac.uk>
2024-02-05 15:39:48 +0000

    DRY

--- app/models/concerns/has_edam_terms.rb
+++ app/models/concerns/has_edam_terms.rb
@@ -2,13 +2,17 @@ module HasEdamTerms
   extend ActiveSupport::Concern
 
   def scientific_topics_and_synonyms
-    scientific_topics.map do |term|
-      [term.preferred_label] + term.has_exact_synonym + term.has_narrow_synonym
-    end.flatten.uniq
+    edam_term_names_and_synonyms(scientific_topics)
   end
 
   def operations_and_synonyms
-    operations.map do |term|
+    edam_term_names_and_synonyms(operations)
+  end
+
+  private
+
+  def edam_term_names_and_synonyms(terms)
+    terms.map do |term|
       [term.preferred_label] + term.has_exact_synonym + term.has_narrow_synonym
     end.flatten.uniq
   end
