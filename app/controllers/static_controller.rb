5bb09262f7d2783a1a2e7f09da916f6dc646ccb9
Mike Sanders <mikesndrs@gmail.com>
2024-04-24 16:08:55 +0200

    catalogue blocks counts

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -23,6 +23,7 @@ class StaticController < ApplicationController
     @featured_trainer = set_featured_trainer
     @events = set_upcoming_events
     @materials = set_latest_materials
+    @catalogue_count_strings = set_catalogue_count_strings
   end
 
   def showcase
@@ -60,5 +61,20 @@ class StaticController < ApplicationController
       sort_by: 'early',
       per_page: 5 * n_events
     ).results.group_by(&:content_provider_id).map { |_p_id, p_events| p_events.first }.first(n_events)
+  end
+
+  def set_catalogue_count_strings
+    catalogue_count_strings = {}
+    return catalogue_count_strings unless TeSS::Config.site.dig('home_page', 'catalogue_counts')
+
+    total_events = Event.pluck(:end).select { |x| x >= Time.zone.now }.count
+    total_events = "#{total_events.round(-2).to_f / 1000}k" if total_events > 100
+    last_month_events = Event.pluck(:created_at).select { |x| x >= Time.zone.now - 1.month }.count
+    catalogue_count_strings['events'] = t('home.catalogue.events_count', total_events:, last_month_events:)
+    catalogue_count_strings['materials'] = t('home.catalogue.materials_count', total_materials: Material.all.count)
+    catalogue_count_strings['workflows'] = t('home.catalogue.workflows_count', total_workflows: Workflow.all.count)
+    catalogue_count_strings['content_providers'] = t('home.catalogue.content_providers_count', total_content_providers: ContentProvider.all.count)
+    catalogue_count_strings['trainers'] = t('home.catalogue.trainers_count', total_trainers: Trainer.all.count)
+    catalogue_count_strings
   end
 end
