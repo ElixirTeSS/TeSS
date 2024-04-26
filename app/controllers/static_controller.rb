610b64d75f6dc4892e52bc148a70c473d63b906f
Mike Sanders <mikesndrs@gmail.com>
2024-04-26 11:22:07 +0200

    show full counts instead of in thousands

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -69,8 +69,6 @@ class StaticController < ApplicationController
 
     total_events = Event.where.not(end: nil).where('events.end > ?', Time.zone.now).count
     last_month_events = Event.where('events.created_at > ?', 1.month.ago).count
-    total_events = "#{total_events.round(-2).to_f / 1000}k" if total_events > 100
-    last_month_events = "#{last_month_events.round(-2).to_f / 1000}k" if last_month_events > 100
     catalogue_count_strings['events'] = t('home.catalogue.events_count', total_events:, last_month_events:)
     # catalogue_count_strings['materials'] = t('home.catalogue.materials_count', total_materials: Material.all.count)
     catalogue_count_strings['materials'] = "#{Material.all.count} #{Material.model_name.human.pluralize.downcase}"
