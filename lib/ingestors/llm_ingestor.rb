f5ee2d48bae4b82d2e3245b1136ae9af00f812a5
Mike Sanders <mikesndrs@gmail.com>
2024-05-31 13:14:14 +0200

    change gpt to llm in scraperee

--- lib/ingestors/llm_ingestor.rb
+++ lib/ingestors/llm_ingestor.rb
@@ -6,15 +6,15 @@ module Ingestors
   class LlmIngestor < Ingestor
     def self.config
       {
-        key: 'gpt_event',
-        title: 'GPT Events API',
+        key: 'llm_event',
+        title: 'LLM Events API',
         category: :events
       }
     end
 
     def read(url)
       begin
-        process_gpt(url)
+        process_llm(url)
       rescue Exception => e
         @messages << "#{self.class.name} failed with: #{e.message}"
       end
@@ -26,24 +26,24 @@ module Ingestors
     private
 
     def scrape_dans
-      sleep(1) unless Rails.env.test? and File.exist?('test/vcr_cassettes/ingestors/gpt.yml')
+      sleep(1) unless Rails.env.test? and File.exist?('test/vcr_cassettes/ingestors/llm.yml')
       url = 'https://dans.knaw.nl/en/agenda/open-hour-ssh-live-qa-on-monday-2/'
       event_page = Nokogiri::HTML5.parse(open_url(url.to_s, raise: true)).css('body').css("div[id='nieuws_detail_row']")
       beep_func(url, event_page)
     end
 
     def scrape_nwo # rubocop:disable Metrics
-      # sleep(1) unless Rails.env.test? and File.exist?('test/vcr_cassettes/ingestors/gpt.yml')
+      # sleep(1) unless Rails.env.test? and File.exist?('test/vcr_cassettes/ingestors/llm.yml')
       # url = 'https://www.nwo.nl/en/meetings/dualis-event-in-utrecht'
       # event_page = Nokogiri::HTML5.parse(open_url(url, raise: true)).css('body').css('main')[0].css('article')
       # beep_func(url, event_page)
       url = 'https://www.nwo.nl/en/meetings'
       4.times.each do |i| # always check the first 4 pages, # of pages could be increased if needed
-        sleep(1) unless Rails.env.test? and File.exist?('test/vcr_cassettes/ingestors/gpt.yml')
+        sleep(1) unless Rails.env.test? and File.exist?('test/vcr_cassettes/ingestors/llm.yml')
         event_page = Nokogiri::HTML5.parse(open_url("#{url}?page=#{i}", raise: true)).css('.overviewContent')[0].css('li.list-item').css('a')
         event_page.each do |event_data|
           new_url = "https://www.nwo.nl#{event_data['href']}"
-          sleep(1) unless Rails.env.test? and File.exist?('test/vcr_cassettes/ingestors/gpt.yml')
+          sleep(1) unless Rails.env.test? and File.exist?('test/vcr_cassettes/ingestors/llm.yml')
           new_event_page = Nokogiri::HTML5.parse(open_url(new_url, raise: true)).css('body').css('main')[0].css('article')
           beep_func(new_url, new_event_page)
         end
@@ -51,7 +51,7 @@ module Ingestors
     end
 
     def scrape_rug # rubocop:disable Metrics
-      # sleep(1) unless Rails.env.test? and File.exist?('test/vcr_cassettes/ingestors/gpt.yml')
+      # sleep(1) unless Rails.env.test? and File.exist?('test/vcr_cassettes/ingestors/llm.yml')
       # url = 'https://www.rug.nl/about-ug/latest-news/events/calendar/2023/phallus-tentoonstelling'
       # event_page = Nokogiri::HTML5.parse(open_url(url.to_s, raise: true)).css('body').css("div[id='main']")[0].css("div[itemtype='https://schema.org/Event']")
       # beep_func(url, event_page)
@@ -60,20 +60,20 @@ module Ingestors
       event_page = Nokogiri::HTML5.parse(open_url(url.to_s, raise: true)).css('body').css("div[id='main']")[0].css("div[itemtype='https://schema.org/Event']")
       event_page.each do |event_data|
         new_url = event_data.css("meta[itemprop='url']")[0].get_attribute('content')
-        sleep(1) unless Rails.env.test? and File.exist?('test/vcr_cassettes/ingestors/gpt.yml')
+        sleep(1) unless Rails.env.test? and File.exist?('test/vcr_cassettes/ingestors/llm.yml')
         new_event_page = Nokogiri::HTML5.parse(open_url(new_url.to_s, raise: true)).css('body').css("div[id='main']")[0].css("div[itemtype='https://schema.org/Event']")
         beep_func(new_url, new_event_page)
       end
     end
 
     def scrape_tdcc
-      sleep(1) unless Rails.env.test? and File.exist?('test/vcr_cassettes/ingestors/gpt.yml')
+      sleep(1) unless Rails.env.test? and File.exist?('test/vcr_cassettes/ingestors/llm.yml')
       url = 'https://tdcc.nl/evenementen/teaming-up-across-domains/'
       event_page = Nokogiri::HTML5.parse(open_url(url.to_s, raise: true)).css('body').css('article')[0]
       beep_func(url, event_page)
     end
 
-    def process_gpt(_url)
+    def process_llm(_url)
       scrape_dans
       scrape_nwo
       scrape_rug
@@ -98,7 +98,7 @@ module Ingestors
         event = llm_service.scrape_func(event, event_page)
         event = llm_service.post_process_func(event)
         event.url = url
-        event.source = 'GPT'
+        event.source = 'LLM'
         event.timezone = 'Amsterdam'
         add_event(event)
       rescue Exception => e
