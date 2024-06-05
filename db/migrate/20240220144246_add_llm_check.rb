b87b3109d9593c6d3937a9748133e80eb5f32943
Mike Sanders <mikesndrs@gmail.com>
2024-06-05 11:26:20 +0200

    migration fix

--- db/migrate/20240220144246_add_llm_check.rb
+++ db/migrate/20240220144246_add_llm_check.rb
@@ -1,28 +1,4 @@
 class AddLlmCheck < ActiveRecord::Migration[7.0]
-  # def up
-  #   create_table :llm_object do |t|
-  #     # t.references :event, foreign_key: true
-  #     t.datetime :created_at
-  #     t.datetime :updated_at
-  #     t.string :scrape_or_process
-  #     t.string :model
-  #     t.string :prompt
-  #     t.string :input
-  #     t.string :output
-  #     t.boolean :needs_processing, default: false
-  #   end
-  #   add_reference :events, :llm_object, foreign_key: true
-  #   add_column :events, :open_science, :string, array: true, default: []
-  #   add_column :materials, :llm_processed, :bool, default: false
-  # end
-
-  # def down
-  #   drop_table :llm_object
-  #   remove_reference :events, :llm_object, foreign_key: true
-  #   remove_column :events, :open_science, :string, array: true, default: []
-  #   remove_column :materials, :llm_processed, :bool, default: false
-  # end
-
   def change
     create_table :llm_objects do |t|
       t.belongs_to :event, foreign_key: true
