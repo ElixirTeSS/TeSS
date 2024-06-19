4e81e5eb9b90061ba9c32ec2a3e4579524b70d05
Finn Bacall <finn.bacall@manchester.ac.uk>
2024-06-19 16:29:17 +0100

    Show warning if GeoIP DB missing

--- lib/locator.rb
+++ lib/locator.rb
@@ -4,11 +4,16 @@ class Locator
   include Singleton
 
   def initialize
+    if database_path.exist?
+      @reader = MaxMind::DB.new(database_path, mode: MaxMind::DB::MODE_MEMORY)
+    else
+      warn "ERROR: No GeoIP database found at #{database_path}. Download from: https://dev.maxmind.com/geoip/geolite2-free-geolocation-data?lang=en"
+    end
     @reader = database_path.exist? ? MaxMind::DB.new(database_path, mode: MaxMind::DB::MODE_MEMORY) : nil
   end
 
   def lookup(ip)
-    @reader.get(ip)
+    @reader&.get(ip)
   end
 
   private
