176fa3ffcf75e3e181d7e24dc507a911b45dabfa
Chris Want <cjwant@gmail.com>
2024-07-09 11:51:43 -0600

    * Configure event languages with `tess.yml'` * remove languages.yml * override language names from I18nData with `I18n.t("languages.#{code}")`

--- app/dictionaries/language_dictionary.rb
+++ app/dictionaries/language_dictionary.rb
@@ -1,39 +1,41 @@
 # Dictionary of Languages
 class LanguageDictionary < Dictionary
 
-  DEFAULT_FILE = 'language.yml'
+  # Not really a dictionary (an array) ...
+  def initialize
+    @dictionary = TeSS::Config.languages
+  end
+
+  def lookup(id)
+    @dictionary.find { |v| v == id }
+  end
 
   # Override because we want this in the viewers language ...
   def options_for_select(existing = nil)
     d = if existing
-          @dictionary.select { |key, _value| existing.include?(key) }
+          @dictionary.select { |key| existing.include?(key) }
         else
           @dictionary
         end
 
-    d.map do |key, _value|
-      [render_language_name(key), key, key]
-    end
+    d.map { |key| [render_language_name(key), key, key] }
   end
 
   def values_for_search(keys)
     return unless keys
-    @dictionary.select { |key, _value| keys.include?(key) }.
-      map { |key, _value| render_language_name(key) }
+    @dictionary.select { |key| keys.include?(key) }.
+      map { |key| render_language_name(key) }
   end
 
   def render_language_name(code)
-    # I18nData lookup wants uppercase key
-    # TODO: is capitalized a good choice for all locales?
-    i18ndata_code = code.to_s.upcase
-    I18nData.languages(I18n.locale)[i18ndata_code].capitalize
+    begin
+      I18n.t("languages.#{code.downcase}", raise: true)
+    rescue I18n::MissingTranslationData
+      # I18nData lookup wants uppercase key
+      # TODO: is capitalized a good choice for all locales?
+      i18ndata_code = code.to_s.upcase
+      I18nData.languages(I18n.locale)[i18ndata_code].capitalize
+    end
   end
-
-  private
-
-  def dictionary_filepath
-    get_file_path 'language', DEFAULT_FILE
-  end
-
 
 end
