c626cc80efa08189d8be2df43e5b9ed6e14aaeab
Finn Bacall <finn.bacall@manchester.ac.uk>
2024-08-29 13:32:04 +0100

    Use "Lax" for cookie "SameSite" restriction, otherwise OAuth2 does not work
    ...because the session resets after the redirect, and so the stored nonce/state is lost.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
-opts = Rails.env.production? ? { same_site: :strict, secure: true } : {}
+opts = Rails.env.production? ? { same_site: :lax, secure: true } : {}
 Rails.application.config.session_store :cookie_store, **opts
