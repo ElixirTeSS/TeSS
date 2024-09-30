07cb0bec94fe5ada75ede1ef29854f7b2acf2012
Finn Bacall <finn.bacall@manchester.ac.uk>
2024-09-30 17:44:56 +0100

    Remove unused counts

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -22,10 +22,10 @@ class CommunitiesController < ApplicationController
     @featured_trainer = nil # set_featured_trainer
     @events = set_upcoming_events
     @materials = set_latest_materials
-    @catalogue_count_strings = set_catalogue_count_strings
   end
 
   private
+
   def set_featured_trainer
     srand(Date.today.beginning_of_day.to_i)
     Trainer.order(:id).sample(1)
@@ -57,23 +57,6 @@ class CommunitiesController < ApplicationController
       sort_by: 'early',
       per_page: 5 * n_events
     ).results.group_by(&:content_provider_id).map { |_p_id, p_events| p_events.first }.first(n_events)
-  end
-
-  def set_catalogue_count_strings
-    catalogue_count_strings = {}
-    return catalogue_count_strings unless TeSS::Config.site.dig('home_page', 'catalogue_counts')
-
-    total_events = Event.where.not(end: nil).where('events.end > ?', Time.zone.now).count
-    last_month_events = Event.where('events.created_at > ?', 1.month.ago).count
-    total_events = "#{total_events.round(-2).to_f / 1000}k" if total_events > 100
-    last_month_events = "#{last_month_events.round(-2).to_f / 1000}k" if last_month_events > 100
-    catalogue_count_strings['events'] = t('home.catalogue.events_count', total_events:, last_month_events:)
-    # catalogue_count_strings['materials'] = t('home.catalogue.materials_count', total_materials: Material.all.count)
-    catalogue_count_strings['materials'] = "#{Material.all.count} #{Material.model_name.human.pluralize.downcase}"
-    catalogue_count_strings['workflows'] = "#{Workflow.all.count} #{Workflow.model_name.human.pluralize.downcase}"
-    catalogue_count_strings['content_providers'] = "#{ContentProvider.all.count} #{ContentProvider.model_name.human.pluralize.downcase}"
-    catalogue_count_strings['trainers'] = "#{Trainer.all.count} #{Trainer.model_name.human.pluralize.downcase}"
-    catalogue_count_strings
   end
 
   def set_community
