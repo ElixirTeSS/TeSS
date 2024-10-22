1ffd96f2032457b17f39c01512147f1082cc52a5
Mike Sanders <mikesndrs@gmail.com>
2024-10-22 13:49:34 +0200

    move extra funcs to class

--- lib/ingestors/oscd_ingestor.rb
+++ lib/ingestors/oscd_ingestor.rb
@@ -63,26 +63,26 @@ module Ingestors
         @messages << "Extract event fields failed with: #{e.message}"
       end
     end
-  end
-end
 
-def oscd_fix_time(date_str)
-  date_str.split(',').each do |str|
-    str.strip.split(' ').each_cons(2) do |el1, el2|
-      next unless is_month?(el1) && el2.to_i.positive?
+    def oscd_fix_time(date_str)
+      date_str.split(',').each do |str|
+        str.strip.split(' ').each_cons(2) do |el1, el2|
+          next unless is_month?(el1) && el2.to_i.positive?
 
-      event_start = Time.zone.parse([el1, el2].join(' '))
-      event_end = Time.zone.parse([el1, el2].join(' '))
-      if event_start < (Time.zone.now - 2.weeks)
-        event_start = event_start.change(year: event_start.year + 1)
-        event_end = event_end.change(year: event_start.year + 1)
+          event_start = Time.zone.parse([el1, el2].join(' '))
+          event_end = Time.zone.parse([el1, el2].join(' '))
+          if event_start < (Time.zone.now - 2.weeks)
+            event_start = event_start.change(year: event_start.year + 1)
+            event_end = event_end.change(year: event_start.year + 1)
+          end
+          return event_start, event_end
+        end
       end
-      return event_start, event_end
+    end
+
+    def is_month?(str)
+      formatted_str = str.strip.capitalize
+      Date::MONTHNAMES.include?(formatted_str) || Date::ABBR_MONTHNAMES.include?(formatted_str)
     end
   end
-end
-
-def is_month?(str)
-  formatted_str = str.strip.capitalize
-  Date::MONTHNAMES.include?(formatted_str) || Date::ABBR_MONTHNAMES.include?(formatted_str)
 end
