60b83fcf765fd617456372f660ed7c8743c585c8
Mike Sanders <mikesndrs@gmail.com>
2024-10-24 21:11:13 +0200

    text referrer instead of string

--- db/migrate/20241022113543_add_referrer_to_widget_logs.rb
+++ db/migrate/20241022113543_add_referrer_to_widget_logs.rb
@@ -1,5 +1,5 @@
 class AddReferrerToWidgetLogs < ActiveRecord::Migration[7.0]
   def change
-    add_column :widget_logs, :referrer, :string
+    add_column :widget_logs, :referrer, :text
   end
 end
