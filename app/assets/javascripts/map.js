ade9e2197118ced28b9c14074a895351d5031e4f
Martin Voigt <m.voigt@hzdr.de>
2025-04-28 11:09:38 +0200

    implement address search for events

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,11 +1,11 @@
 class GoogleMap {
-    constructor({ center, dom_element }) {
-        this.marker_bounds = new google.maps.LatLngBounds();
+    constructor({ center, dom_element, zoom }) {
+        this.markers = [];
         this.infowindow = new google.maps.InfoWindow({content: content});
         this.map = new google.maps.Map(dom_element, {
-            center,
+            center: new google.maps.LatLng(center.lat, center.lng),
             scrollwheel: true,
-            zoom: 13,
+            zoom: zoom || 13,
             maxZoom: 17
         });
     }
@@ -15,7 +15,7 @@ class GoogleMap {
             map: this.map,
             position: location,
             title,
-            icon
+            icon: {url: icon, scaledSize: new google.maps.Size(35, 35)}
         });
         if (description) {
             google.maps.event.addListener(marker, 'click', function () {
@@ -23,25 +23,105 @@ class GoogleMap {
                 infowindow.open(EventsMap.map, marker);
             });
         }
-        this.marker_bounds.extend(marker.position);
-        return marker;
+        this.markers.push(marker);
+    }
+
+    delete_markers() {
+        this.infowindow.close();
+        this.markers.forEach((m) => m.setMap(null));
+        this.markers = [];
     }
 
     fit_to_markers() {
-        this.map.fitBounds(this.marker_bounds);
+        var marker_bounds = new google.maps.LatLngBounds();
+        this.markers.forEach((m) => marker_bounds.extend(m.position));
+        this.map.fitBounds(marker_bounds);
+    }
+
+    make_address_finder({address_input, callback}) {
+        var autocomplete = new google.maps.places.Autocomplete(address_input);
+        autocomplete.bindTo('bounds', this.map);
+        autocomplete.addListener('place_changed', () => {
+            var place = autocomplete.getPlace();
+            if (!place.geometry) {
+                window.alert("Couldn't find that location on the map");
+                return;
+            }
+
+            // If the place has a geometry, then present it on a map.
+            if (place.geometry.viewport) {
+                this.map.fitBounds(place.geometry.viewport);
+            } else {
+                this.map.setCenter(place.geometry.location);
+                this.map.setZoom(17);  // Why 17? Because it looks good.
+            }
+
+            var address_info = {
+                venue: place.name,
+                city: null,
+                country: null,
+                postcode: null,
+                lat: place.geometry.location.lat(),
+                lng: place.geometry.location.lng()
+            };
+            var street_number = '';
+            var street_short = '';
+            var street_name = '';
+
+            // Extract address fields
+            for (var i = 0; i < place.address_components.length; i++) {
+                var addressType = place.address_components[i].types[0];
+                switch (addressType) {
+                    case 'street_number':
+                        street_number = place.address_components[i].short_name;
+                        break;
+                    case 'route':
+                        street_name = place.address_components[i].long_name;
+                        street_short = place.address_components[i].short_name;
+                        break;
+                    case 'locality':
+                    case 'postal_town':
+                        address_info.city = place.address_components[i].long_name;
+                        break;
+                    case 'administrative_area_level_2':
+                        address_info.country = place.address_components[i].long_name;
+                        break;
+                    case 'country':
+                        address_info.country = place.address_components[i].short_name;
+                        break;
+                    case 'postal_code':
+                        address_info.postcode = place.address_components[i].short_name;
+                        break;
+                }
+            }
+
+            // Update venue with street address
+            var street_address_short = street_number + ' ' + street_short;
+            var street_address_long = street_number + ' ' + street_name;
+            if (street_address_long.trim().length > 0 && address_info.venue !== street_address_long) {
+                if (address_info.venue === street_address_short) {
+                    address_info.venue = street_address_long;
+                } else {
+                    address_info.venue = place.name + ', ' + street_address_long;
+                }
+            }
+
+            callback(address_info)
+        })
     }
 }
 
 
 class OpenStreetMap {
-    constructor({ center, dom_element }) {
-        this.marker_points = [];
+    constructor({ center, dom_element, zoom }) {
+        this.markers = [];
+        this.info_windows = [];
         this.map = new ol.Map({
             target: dom_element,
             layers: [new ol.layer.Tile({ source: new ol.source.OSM() })],
             view: new ol.View({
                 center: ol.proj.fromLonLat([center.lng, center.lat]),
-                zoom: 13,
+                zoom: zoom || 13,
                 maxZoom: 17
             })
         });
@@ -49,21 +129,23 @@ class OpenStreetMap {
 
     add_marker({ location, title, icon, description, link }) {
         icon = icon || 'https://pan-training.eu/events/marker.png';
-        var marker = $('<img width="50" height="50" style="cursor: pointer;">');
+        var marker = $('<img width="35" height="35" style="cursor: pointer;">');
         marker.prop("src", icon);
         marker.prop("title", title);
         var point = ol.proj.fromLonLat([location.lng, location.lat])
-        this.map.addOverlay(new ol.Overlay({
+        var overlay = new ol.Overlay({
             position: point,
-            offset: [-25, -50],
+            offset: [-17, -35],
             element: marker[0]
-        }));
+        });
+        this.map.addOverlay(overlay);
         
         if (description) {
             var popup = $('<div class="ol-popup"><a class="ol-popup-closer" href="#"></a><div class="ol-popup-content"></div></div>').hide();
             popup.children(".ol-popup-content").html(description);
             
             var infowindow = new ol.Overlay({element: popup[0], offset: [10, 10]});
+            this.info_windows.push(infowindow);
             this.map.addOverlay(infowindow);
             infowindow.setPosition(point);
             marker.on('click', () => {
@@ -78,16 +160,73 @@ class OpenStreetMap {
         if (link) {
             marker.on('dblclick', () => {window.open(link, '_self')})
         }
-        this.marker_points.push(point);
+        this.markers.push(overlay)
+    }
+
+    delete_markers() {
+        this.markers.forEach(m => this.map.removeOverlay(m));
+        this.markers = [];
+        this.info_windows.forEach(info => this.map.removeOverlay(info));
+        this.info_windows = [];
     }
 
     fit_to_markers() {
         var fit_todo = true;
         this.map.on('loadend', () => {
-            if(fit_todo) this.map.getView().fit(new ol.geom.MultiPoint(this.marker_points), { padding: [50, 50, 50, 50] });
+            if(fit_todo) this.map.getView().fit(
+                new ol.geom.MultiPoint(this.markers.map(m => m.getPosition())), 
+                { padding: [50, 50, 50, 50] }
+            );
             fit_todo = false;
         });
     }
+
+    make_address_finder({address_input, callback}) {
+        $(address_input).hide();
+        var geocoder = new Geocoder('nominatim', {
+            provider: 'osm',
+            lang: $('html').prop('lang') || 'en',
+            preventMarker: true,
+            keepOpen: true,
+        });
+        this.map.addControl(geocoder);
+        geocoder.on('addresschosen', (evt) => {
+            var address_info = {
+                venue: evt.address.details.name,
+                city: evt.address.details.city,
+                country: evt.address.details.country,
+                postcode: evt.address.details.postcode,
+                lat: evt.place.lat,
+                lng: evt.place.lon
+            };
+            callback(address_info);
+        });
+        this.map.on('click', (evt) => {
+            var coords = ol.proj.transform(evt.coordinate, 'EPSG:3857', 'EPSG:4326');
+            var request_data = {
+                lon: coords[0],
+                lat: coords[1],
+                format: 'json',
+                limit: 1
+            }
+            $.get("https://nominatim.openstreetmap.org/reverse", request_data, response => {
+                var address_info = {
+                    venue: response.display_name,
+                    city: response.address.city || response.address.town || response.address.village,
+                    country: response.address.country_code.toUpperCase(),
+                    postcode: response.address.postcode,
+                    lat: request_data.lat,
+                    lng: request_data.lon
+                };
+                callback(address_info);
+            });
+        })
+    }
+}
+
+
+function get_map_class(provider) {
+    return provider == 'google' ? GoogleMap : OpenStreetMap;
 }
 
 
@@ -108,7 +247,7 @@ var EventMap = {
                 lng: parseFloat(element.data('mapSuggestedLongitude'))
             };
 
-            var MapClass = element.data("map-provider") == 'google' ? GoogleMap : OpenStreetMap;
+            var MapClass = get_map_class(element.data("map-provider"));
             var map = new MapClass({
                 center: suggestedLocation.lat ? suggestedLocation : actualLocation,
                 dom_element: element[0]
@@ -203,7 +342,7 @@ var EventsMap = {
         $('#map-notice').hide();
         $('#map-loading-screen').fadeIn();
 
-        var MapClass = element.data('provider') == 'google' ? GoogleMap : OpenStreetMap;
+        var MapClass = get_map_class(element.data('provider'));
         EventsMap.map = new MapClass({
             center: { lat: 0, lng: 0 },
             dom_element: document.getElementById('map-canvas')
