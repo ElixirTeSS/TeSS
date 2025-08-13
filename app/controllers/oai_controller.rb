8531f82d88b261072bb8e670edf252a525424403
Martin Voigt <m.voigt@hzdr.de>
2025-08-13 15:16:44 +0200

    Add XSLT formating of OAI-PMH XML Results

--- app/controllers/oai_controller.rb
+++ app/controllers/oai_controller.rb
@@ -2,6 +2,10 @@ class OaiController < ApplicationController
   def index
     provider = TrainingProvider.new
     response =  provider.process_request(oai_params.to_h)
+
+    # add XSLT prefix
+    response.sub!(/<\?xml[^>]+\?>/, "\\0\n<?xml-stylesheet type=\"text/xsl\" href=\"/oai2xhtml.xsl\"?>")
+
     render :body => response, :content_type => 'text/xml'
   end
 
