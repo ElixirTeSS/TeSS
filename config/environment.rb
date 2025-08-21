38e162288d9d3c06b7177c0407705077a64a6142
Martin Voigt <m.voigt@hzdr.de>
2025-08-21 14:48:35 +0200

    Set repository_url of OAI-PMH based on base_url

--- config/environment.rb
+++ config/environment.rb
@@ -19,7 +19,7 @@ end
 
 class TrainingProvider < OAI::Provider::Base
   repository_name TeSS::Config.site['title']
-  repository_url 'http://localhost:3000/oai-pmh'
+  repository_url "#{URI(TeSS::Config.base_url).host}/oai-pmh"
   record_prefix "oai:#{URI(TeSS::Config.base_url).host}"
   admin_email TeSS::Config.contact_email
   source_model OAI::Provider::ActiveRecordWrapper.new(Material)
