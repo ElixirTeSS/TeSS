ba745c9d5ba77c08214aea788ae674f1786307c2
Finn Bacall <finn.bacall@manchester.ac.uk>
2025-12-11 10:15:08 +0000

    Fix bad ORCIDs in tests

--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -48,16 +48,16 @@ class PersonTest < ActiveSupport::TestCase
 
   test 'should automatically link to profile by orcid on save' do
     profile = profiles(:trainer_one_profile)
-    # The trainer_one_profile has orcid: https://orcid.org/000-0002-1825-0097
-    person = Person.create(full_name: 'Josiah Carberry', orcid: 'https://orcid.org/000-0002-1825-0097')
+    # The trainer_one_profile has orcid: https://orcid.org/0000-0002-1825-0097
+    person = Person.create(full_name: 'Josiah Carberry', orcid: 'https://orcid.org/0000-0002-1825-0097')
     assert person.valid?
     assert_equal profile, person.profile
   end
 
   test 'should automatically link to profile using short orcid format' do
     profile = profiles(:trainer_one_profile)
-    # The trainer_one_profile has orcid: https://orcid.org/000-0002-1825-0097
-    person = Person.create(full_name: 'Josiah Carberry', orcid: '000-0002-1825-0097')
+    # The trainer_one_profile has orcid: https://orcid.org/0000-0002-1825-0097
+    person = Person.create(full_name: 'Josiah Carberry', orcid: '0000-0002-1825-0097')
     assert person.valid?
     assert_equal profile, person.profile
   end
@@ -68,6 +68,18 @@ class PersonTest < ActiveSupport::TestCase
     assert_nil person.profile
   end
 
+  test 'should should break profile link if orcid removed' do
+    profile = profiles(:trainer_one_profile)
+    # The trainer_one_profile has orcid: https://orcid.org/0000-0002-1825-0097
+    person = Person.create(full_name: 'Josiah Carberry', orcid: '0000-0002-1825-0097', profile: profile)
+    assert person.valid?
+    assert_equal profile, person.profile
+
+    person.update(orcid: nil)
+    assert_nil person.profile
+  end
+
+
   test 'should not override existing profile link' do
     profile1 = profiles(:trainer_one_profile)
     profile2 = profiles(:admin_trainer_profile)
@@ -77,7 +89,7 @@ class PersonTest < ActiveSupport::TestCase
     assert_equal profile2, person.profile
 
     # Even if we update the ORCID to match profile1, it should keep profile2
-    person.update(orcid: 'https://orcid.org/000-0002-1825-0097')
+    refute person.update(orcid: '0000-0002-1825-0097')
     assert_equal profile2, person.profile
   end
 end
