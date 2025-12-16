70392c7dc5bc4d4bc078fb52c67ea07ee7bea295
kennethrioja <59597207+kennethrioja@users.noreply.github.com>
2025-12-16 16:48:27 +0100

    review(#1194): removing test, unless blank instead of nil

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,7 +4,7 @@ opts = {}
 if Rails.env.production?
   opts = { same_site: :lax, secure: true }
   expiry_time = TeSS::Config.login_expires_after
-  opts[:expire_after] = expiry_time unless expiry_time.nil?
+  opts[:expire_after] = expiry_time unless expiry_time.blank?
 end
 
 Rails.application.config.session_store :cookie_store, **opts
