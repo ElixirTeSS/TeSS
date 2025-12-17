4142959fa638f7626e3ea605354e61b26c01587a
Martin Voigt <m.voigt@hzdr.de>
2025-12-17 17:32:56 +0100

    #1192 Support more kinds of exchange filters

--- app/models/source_filter.rb
+++ app/models/source_filter.rb
@@ -1,9 +1,30 @@
 class SourceFilter < ApplicationRecord
   belongs_to :source
 
+  auto_strip_attributes :filter_value
+
   enum :filter_by, {
     target_audience: 'target_audience',
-    keyword: 'keyword'
+    keyword: 'keyword',
+    title: 'title',
+    description: 'description',
+    description_contains: 'description_contains',
+    id: 'id',
+    url: 'url',
+    url_prefix: 'url_prefix',
+    doi: 'doi',
+    license: 'license',
+    difficulty_level: 'difficulty_level',
+    resource_type: 'resource_type',
+    prerequisites_contains: 'prerequisites_contains',
+    learning_objectives_contains: 'learning_objectives_contains',
+    subtitle: 'subtitle',
+    subtitle_contains: 'subtitle_contains',
+    city: 'city',
+    country: 'country',
+    presence: 'presence',
+    event_type: 'event_type',
+    timezone: 'timezone'
   }
 
   enum :filter_mode, {
@@ -14,17 +35,38 @@ class SourceFilter < ApplicationRecord
   def match(item)
     return false unless item.respond_to?(filter_property)
 
-    # array properties
-    return unless %w[target_audience keywords].include?(filter_property)
+    val = item.send(filter_property)
 
-    item.send(filter_property).any? do |i|
-      i == filter_value
+    # string match
+    if %w[title id url doi description license difficulty_level subtitle city country].include?(filter_by)
+      val.to_s.casecmp(filter_value)
+    # prefix string match
+    elsif %w[url_prefix].include?(filter_by)
+      val.to_s.downcase.start_with?(filter_value.downcase)
+    # contains string match
+    elsif %w[description_contains prerequisites_contains subtitle_contains].include?(filter_by)
+      val.to_s.downcase.include?(filter_value.downcase)
+    # array string match
+    elsif %w[target_audience keyword resource_type event_type].include?(filter_by)
+      val.any? { |i| i.to_s.casecmp?(filter_value) }
+    # boolean match
+    elsif %w[presence].include?(filter_by)
+      bool_map = { 'yes' => 'true', 'no' => 'false' }
+      (val != 0).to_s.casecmp(bool_map.fetch(filter_value.downcase, filter_value))
+    else
+      false
     end
   end
 
   def filter_property
-    return 'keywords' if keyword?
-
-    filter_by
+    {
+      'event_type' => 'event_types',
+      'keyword' => 'keywords',
+      'url_prefix' => 'url',
+      'description_contains' => 'description',
+      'prerequisites_contains' => 'prerequisites',
+      'learning_objectives_contains' => 'learning_objectives',
+      'subtitle_contains' => 'subtitle'
+    }.fetch(filter_by, filter_by)
   end
 end
