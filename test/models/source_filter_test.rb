6b3bce600ac6fcc1c5e389b30fc80bcbdc532f88
Martin Voigt <m.voigt@hzdr.de>
2026-01-09 11:33:57 +0100

    Add comment where to find source filter tests.

--- test/models/source_filter_test.rb
+++ test/models/source_filter_test.rb
@@ -1,6 +1,8 @@
-require "test_helper"
+require 'test_helper'
 
 class SourceFilterTest < ActiveSupport::TestCase
+  # The source_filter model is tested extensively as part of the scraper_test ingestor unit test.
+
   # test "the truth" do
   #   assert true
   # end
