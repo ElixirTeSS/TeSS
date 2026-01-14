2f4b272be6554cd5e0941718a54204c9081378fc
Martin Voigt <m.voigt@hzdr.de>
2026-01-14 15:30:14 +0100

    Use CamelCase in source_filters.js

--- app/assets/javascripts/source_filters.js
+++ app/assets/javascripts/source_filters.js
@@ -1,19 +1,19 @@
 var SourceFilters = {
-    last_id: function () {
+    lastId: function () {
         var existing_list_item_ids = $(".source-filter-form").map(function (i, c) { return $(c).data("id-in-filter-list") });
         if (existing_list_item_ids.length == 0) return 0;
         return Math.max.apply(null, existing_list_item_ids) + 1;
     },
 
     add: function () {
-        var new_form = $($('#source-filter-template').clone().html().replace(/REPLACE_ME/g, SourceFilters.last_id()));
+        var new_form = $($('#source-filter-template').clone().html().replace(/REPLACE_ME/g, SourceFilters.lastId()));
         new_form.appendTo('#source-filter-list');
 
         return false; // Stop form being submitted
     },
 
-    add_block_filter: function () {
-        var new_form = $($('#source-filter-template').clone().html().replace(/REPLACE_ME/g, SourceFilters.last_id()).replace(/allow/, 'block'));
+    addBlockFilter: function () {
+        var new_form = $($('#source-filter-template').clone().html().replace(/REPLACE_ME/g, SourceFilters.lastId()).replace(/allow/, 'block'));
         new_form.appendTo('#source-block-list');
 
         return false; // Stop form being submitted
@@ -30,7 +30,7 @@ document.addEventListener("turbolinks:load", function () {
         .on('click', '#add-source-filter-btn-label', SourceFilters.add)
         .on('click', '.delete-source-filter-btn', SourceFilters.delete);
     $('#source-block-filters')
-        .on('click', '#add-source-block-filter-btn', SourceFilters.add_block_filter)
-        .on('click', '#add-source-block-filter-btn-label', SourceFilters.add_block_filter)
+        .on('click', '#add-source-block-filter-btn', SourceFilters.addBlockFilter)
+        .on('click', '#add-source-block-filter-btn-label', SourceFilters.addBlockFilter)
         .on('click', '.delete-source-filter-btn', SourceFilters.delete);
 });
