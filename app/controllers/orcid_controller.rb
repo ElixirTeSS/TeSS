1ef62e3c35878237a08dac72299390361aa000c8
Finn Bacall <finn.bacall@manchester.ac.uk>
2026-02-19 14:48:56 +0000

    Remove duplicate definition

--- app/controllers/orcid_controller.rb
+++ app/controllers/orcid_controller.rb
@@ -40,17 +40,6 @@ class OrcidController < ApplicationController
 
   private
 
-  def redirect_to_space(path, space)
-    if space&.is_subdomain?
-      port_part = ''
-      port_part = ":#{request.port}" if (request.protocol == "http://" && request.port != 80) ||
-        (request.protocol == "https://" && request.port != 443)
-      redirect_to URI.join("#{request.protocol}#{space.host}#{port_part}", path).to_s, allow_other_host: true
-    else
-      redirect_to path
-    end
-  end
-
   def set_oauth_client
     config = Rails.application.config.secrets.orcid
     @oauth2_client ||= Rack::OAuth2::Client.new(
