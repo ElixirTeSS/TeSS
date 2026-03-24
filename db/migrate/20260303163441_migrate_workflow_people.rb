8241ac004dec787340df5bd9144ce357d9c2e0df
Finn Bacall <finn.bacall@manchester.ac.uk>
2026-03-24 13:36:53 +0000

    Fix broken people migrations

--- db/migrate/20260303163441_migrate_workflow_people.rb
+++ db/migrate/20260303163441_migrate_workflow_people.rb
@@ -1,4 +1,8 @@
+require_relative '../migration_helper'
+
 class MigrateWorkflowPeople < ActiveRecord::Migration[7.2]
+  include MigrationHelper
+
   class Person < ActiveRecord::Base; end unless defined?(Person)
 
   def up
@@ -21,35 +25,5 @@ class MigrateWorkflowPeople < ActiveRecord::Migration[7.2]
       print '.'
     end
     puts
-  end
-
-  private
-
-  def array_to_people(resource, column_name, role)
-    return unless resource.respond_to?(:read_attribute)
-    # Get the raw array from database
-    people_array = resource.read_attribute(column_name)
-    return if people_array.blank?
-
-    people_array.each do |person_name|
-      next if person_name.blank?
-      orcid = nil
-      name = person_name.gsub(/\s*\(?(orcid: )?(https?:\/\/orcid\.org\/)?(\d\d\d\d-\d\d\d\d-\d\d\d\d-\d\d\d[\dxX])[ \)]*/) do |_|
-        orcid = $3
-        ''
-      end.strip
-
-      Person.find_or_create_by!(full_name: name, orcid: orcid, resource: resource, role: role)
-    end
-  end
-
-  def people_to_array(resource, column_name, role)
-    arr = Person.where(resource: resource, role: role).map do |person|
-      name = person.full_name
-      name += " (#{person.orcid})" if person.orcid
-      name
-    end
-
-    resource.update_column(column_name, arr)
   end
 end
