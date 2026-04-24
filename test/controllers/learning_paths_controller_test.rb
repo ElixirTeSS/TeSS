768c5de3f9b838d97d0b6bf022d1306463db2dec
Finn Bacall <finn.bacall@manchester.ac.uk>
2026-04-24 12:53:56 +0100

    Test for unordered display

--- test/controllers/learning_paths_controller_test.rb
+++ test/controllers/learning_paths_controller_test.rb
@@ -152,7 +152,10 @@ class LearningPathsControllerTest < ActionController::TestCase
     assert_response :success
     assert assigns(:learning_path)
     assert_select '.learning-path-topic', count: 2
+    assert_operator assigns(:learning_path).topics.length, :>, 1
     assert_select '.learning-path-topic.single-topic', count: 0
+    refute assigns(:learning_path).unordered?
+    assert_select '.learning-path-topic.unordered', count: 0
   end
 
   test 'should show learning_path as json' do
@@ -570,5 +573,15 @@ class LearningPathsControllerTest < ActionController::TestCase
     assert assigns(:learning_path)
     assert_select '.learning-path-topic', count: 1
     assert_select '.learning-path-topic.single-topic', count: 1
+  end
+
+  test 'displays unordered view when learning path is unordered' do
+    learning_path = learning_paths(:one)
+    learning_path.update!(unordered: true)
+    get :show, params: { id: learning_path }
+    assert_response :success
+    assert assigns(:learning_path)
+    assert assigns(:learning_path).unordered?
+    assert_select '.learning-path-topic.unordered', minimum: 1
   end
 end
