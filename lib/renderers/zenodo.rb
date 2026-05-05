7e90a2a5601d3a05856eac115092bda3d381007c
Martin Voigt <m.voigt@hzdr.de>
2026-05-05 11:16:38 +0200

    Remove unused VALID_HOSTS variable in Zenodo renderer

--- lib/renderers/zenodo.rb
+++ lib/renderers/zenodo.rb
@@ -1,12 +1,15 @@
 module Renderers
   class Zenodo
-    VALID_HOSTS = %w[zenodo.org doi.org].freeze
     VALID_SCHEMES = %w[http https].freeze
-    TEMPLATE = %(<video controls height="500" style="display:none;" id="zenodo-video"></video><script>make_zenodo_video(document.getElementById('zenodo-video'), '%{files_url}', %{key});</script>)
+    TEMPLATE = %(<video controls height="500" style="display:none;" id="zenodo-video"></video><script>make_zenodo_video(document.getElementById('zenodo-video'), '%<files_url>s', %<key>s);</script>)
 
     def initialize(resource)
       @url = resource.url
-      @parsed_url = Addressable::URI.parse(@url) rescue nil
+      @parsed_url = begin
+        Addressable::URI.parse(@url)
+      rescue StandardError
+        nil
+      end
     end
 
     def can_render?
@@ -16,14 +19,14 @@ module Renderers
     def render_content
       files_url = "https://zenodo.org/api/records/#{zenodo_id}/files"
       key = @parsed_url.query_values.to_h['preview_file']
-      (TEMPLATE % { files_url: files_url, key: key.to_json }).html_safe
+      format(TEMPLATE, files_url:, key: key.to_json).html_safe
     end
 
     private
 
     def zenodo_id
-      match = @parsed_url.host == 'zenodo.org' && @url.match(/records\/(\d+)/) ||
-        @parsed_url.host == 'doi.org' && @url.match(/10\.5281\/zenodo\.(\d+)/)
+      match = @parsed_url.host == 'zenodo.org' && @url.match(%r{records/(\d+)}) ||
+              @parsed_url.host == 'doi.org' && @url.match(%r{10\.5281/zenodo\.(\d+)})
       match[1] if match
     end
   end
