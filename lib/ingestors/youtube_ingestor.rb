b7196b91b425578e9fe855990e3586908530b6e2
Martin Voigt <m.voigt@hzdr.de>
2026-05-22 10:23:53 +0200

    Separate youtube ingestor

---
+++ lib/ingestors/youtube_ingestor.rb
@@ -0,0 +1,34 @@
+module Ingestors
+  class YoutubeIngestor < MaterialRSSIngestor
+    require 'cgi'
+
+    def self.config
+      {
+        key: 'youtube',
+        title: 'YouTube',
+        category: :materials
+      }
+    end
+
+    def discover_feed_url(content, base_url)
+      super(content, base_url)
+
+      return unless (url = discover_feed_url_from_youtube_playlist_url(base_url))
+
+      @messages << "Found Atom feed link from YouTube playlist URL, following: #{url}"
+      url
+    end
+
+    def discover_feed_url_from_youtube_playlist_url(base_url)
+      uri = URI.parse(base_url)
+      return nil unless Renderers::Youtube.is_youtube_url?(base_url)
+
+      playlist_id = CGI.parse(uri.query.to_s).fetch('list', []).first
+      return nil if playlist_id.blank?
+
+      "https://www.youtube.com/feeds/videos.xml?playlist_id=#{CGI.escape(playlist_id)}"
+    rescue URI::InvalidURIError
+      nil
+    end
+  end
+end
