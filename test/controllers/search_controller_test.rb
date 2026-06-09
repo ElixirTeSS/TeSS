079590703f8c7ce3901f7039ffcfec9a6c2aa5f6
Finn Bacall <finn.bacall@manchester.ac.uk>
2026-06-09 12:21:21 +0100

    Test for space restriction on search

--- test/controllers/search_controller_test.rb
+++ test/controllers/search_controller_test.rb
@@ -46,4 +46,63 @@ class SearchControllerTest < ActionController::TestCase
     end
   end
 
+  test 'should restrict searches to current space' do
+    plant_space = spaces(:plants)
+    # Dummy class that records which `with` constraints were used in the search
+    dummy_search = Class.new do
+      attr_reader :constraints
+
+      def initialize
+        @constraints = Hash.new(:unset)
+      end
+
+      def clear
+        @constraints.clear
+      end
+
+      def with(field, value = nil)
+        @constraints[field] = value
+        self
+      end
+
+      def method_missing(*args); self; end
+    end
+
+    search = dummy_search.new
+    Sunspot.stub(:search, -> (_, &block) { search.instance_eval(&block) }) do
+      # When not in a space, with spaces disabled, space constraint should be unset (show everything)
+      with_settings(solr_enabled: true, feature: { spaces: false }) do
+        get :index, params: { q: 'banana' }
+        assert_response :success
+        assert_equal :unset, search.constraints[:space_id]
+      end
+
+      search.clear
+      # When not in a space, with spaces enabled, space constraint should be nil (show things in default space)
+      with_settings(solr_enabled: true, feature: { spaces: true }) do
+        get :index, params: { q: 'banana' }
+        assert_response :success
+        assert_nil search.constraints[:space_id]
+      end
+
+      with_host(plant_space.host) do
+        search.clear
+        # When in a space, with spaces enabled, space constraint should be that space's ID (show things in that space)
+        with_settings(solr_enabled: true, feature: { spaces: true }) do
+          get :index, params: { q: 'banana' }
+          assert_response :success
+          assert_equal plant_space.id, search.constraints[:space_id]
+        end
+
+        search.clear
+        # When in a space, with spaces disabled, space constraint should be unset (show everything)
+        with_settings(solr_enabled: true, feature: { spaces: false }) do
+          get :index, params: { q: 'banana' }
+          assert_response :success
+          assert_equal :unset, search.constraints[:space_id]
+        end
+      end
+    end
+  end
+
 end
