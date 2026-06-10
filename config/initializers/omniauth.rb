2f4d08064dd42670adecb25ea66ea14d66273465
Finn Bacall <finn.bacall@manchester.ac.uk>
2026-06-10 12:05:01 +0100

    Skip omniauth CSRF protection in tests

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   OmniAuth.config.allowed_request_methods = [:post]
 
-  OmniAuth.config.request_validation_phase = OmniAuth::AuthenticityTokenProtection.new(key: :_csrf_token)
+  OmniAuth.config.request_validation_phase = Rails.env.test? ? nil : OmniAuth::AuthenticityTokenProtection.new(key: :_csrf_token)
 end
 
