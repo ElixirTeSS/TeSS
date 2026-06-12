7b4899cb2770be18c92631f911cec81978f0a86e
Valentin Ryckaert <valentin.ryckaert@protonmail.com>
2026-06-12 13:45:15 +0200

    feat: can add groups to a space in space form

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -76,7 +76,7 @@ class SpacesController < ApplicationController
   end
 
   def space_params
-    permitted = [:title, :description, :theme, :image, :image_url, { administrator_ids: [] }, { enabled_features: [] }]
+    permitted = [:title, :description, :theme, :image, :image_url, { administrator_ids: [] }, { enabled_features: [] }, { group_ids: [] }]
     permitted += [:host] if current_user.is_admin?
     params.require(:space).permit(*permitted)
   end
