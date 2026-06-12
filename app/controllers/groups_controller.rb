8b5485a1167ac5a61a3483e94c031643f9e97391
Valentin Ryckaert <valentin.ryckaert@protonmail.com>
2026-06-12 09:29:19 +0200

    feat: add policy to group controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,35 +1,35 @@
 class GroupsController < ApplicationController
   before_action :set_group, only: %i[ show edit update destroy ]
 
-  # GET /groups or /groups.json
+  # GET /groups
   def index
     @groups = Group.all
   end
 
-  # GET /groups/1 or /groups/1.json
+  # GET /groups/1
   def show
   end
 
   # GET /groups/new
   def new
+    authorize Group
     @group = Group.new
   end
 
   # GET /groups/1/edit
   def edit
+    authorize @group
   end
 
-  # POST /groups or /groups.json
+  # POST /groups
   def create
     @group = Group.new(group_params)
 
     respond_to do |format|
       if @group.save
         format.html { redirect_to @group, notice: "Group was successfully created." }
-        format.json { render :show, status: :created, location: @group }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @group.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -39,21 +39,19 @@ class GroupsController < ApplicationController
     respond_to do |format|
       if @group.update(group_params)
         format.html { redirect_to @group, notice: "Group was successfully updated." }
-        format.json { render :show, status: :ok, location: @group }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @group.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /groups/1 or /groups/1.json
   def destroy
+    authorize @group
     @group.destroy!
 
     respond_to do |format|
       format.html { redirect_to groups_path, status: :see_other, notice: "Group was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
