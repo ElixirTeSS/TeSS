e944ddf11d2968591744f0fe1520a4a508490210
Valentin Ryckaert <valentin.ryckaert@protonmail.com>
2026-06-19 15:01:16 +0200

    fix: cleanly add js script for private groups in space form

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,4 @@
 //= link_tree ../images
 //= link_directory ../stylesheets/themes .css
 //= link application.js
+//= link show_private_groups.js
---
+++ app/assets/javascripts/show_private_groups.js
@@ -0,0 +1,21 @@
+// excuted for space form
+function show_private_groups() {
+    let checkbox = document.getElementById("space_is_private")
+    let container = document.getElementById("groups_container")
+
+    const toggleGroups = () => {
+        if (checkbox && checkbox.checked) {
+            container.style.display = "block"
+        } else {
+            container.style.display = "none"
+        }
+    }
+
+    if (checkbox) {
+        checkbox.addEventListener("change", toggleGroups)
+    }
+}
+
+window.addEventListener('turbolinks:load', function() {
+    show_private_groups();
+});
