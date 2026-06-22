8ee41f5911c72f36fc60f67b3e723e896201b27c
Finn Bacall <finn.bacall@manchester.ac.uk>
2026-06-22 15:27:01 +0100

    ????

--- scripts/check_deployment.rb
+++ scripts/check_deployment.rb
@@ -2,16 +2,20 @@ require 'json'
 
 output = `curl --verbose --silent http://localhost:3000/ 2>&1`
 
+puts 0
 if $?.success? && output.include?('Browse the catalogue')
   docker_ps = `docker compose ps -a --format json | jq -s`
+  puts 1
   if $?.success?
+    puts 2
     j = JSON.parse(docker_ps)
     unless j.any? { |c| c['ExitCode'] == 1 }
+      puts 'exit 0'
       exit 0
     end
   end
 end
-
+puts 3
 puts "::group::Docker ps"
 puts `docker compose ps -a`
 puts "::endgroup::"
