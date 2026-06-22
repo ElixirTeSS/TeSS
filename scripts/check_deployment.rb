1efe52c83fca72c105585d9ea3505cf541d9bec3
Finn Bacall <finn.bacall@manchester.ac.uk>
2026-06-22 10:30:55 +0100

    Check `docker compose ps` for dead containers

--- scripts/check_deployment.rb
+++ scripts/check_deployment.rb
@@ -1,13 +1,23 @@
 output = `curl --verbose --silent http://localhost:3000/ 2>&1`
+
 if $?.success? && output.include?('Browse the catalogue')
-  exit 0
-else
-  puts "::group::Docker logs"
-  puts `docker compose --file docker-compose-prod.yml logs`
-  puts "::endgroup::"
-  puts "::group::curl output"
-  puts output
-  puts "::endgroup::"
-  exit 1
+  docker_ps = `docker compose ps -a --format json`
+  if $?.success?
+    j = JSON.parse(docker_ps)
+    unless j.any? { |c| c['ExitCode'] == 1 }
+      exit 0
+    end
+  end
 end
 
+puts "::group::Docker ps"
+puts `docker compose ps -a`
+puts "::endgroup::"
+puts "::group::Docker logs"
+puts `docker compose --file docker-compose-prod.yml logs`
+puts "::endgroup::"
+puts "::group::curl output"
+puts output
+puts "::endgroup::"
+
+exit 1
