32b862b80befa983ec665f4707c19fb655bd3090
Finn Bacall <finn.bacall@manchester.ac.uk>
2026-06-23 09:32:48 +0100

    Revert debugging

--- scripts/check_deployment.rb
+++ scripts/check_deployment.rb
@@ -1,40 +1,13 @@
-require 'json'
-
 output = `curl --verbose --silent http://localhost:3000/ 2>&1`
-
-puts 0
 if $?.success? && output.include?('Browse the catalogue')
-  docker_ps = `docker compose ps -a --format json | jq -s`
-  puts 1
-  if $?.success?
-    puts 2
-    j = JSON.parse(docker_ps)
-    unless j.any? { |c| c['ExitCode'] == 1 } || j.any? { |c| c['Health'] == 'unhealthy' }
-      puts 'exit 0'
-      puts "::group::Docker ps"
-      puts `docker compose ps -a`
-      puts "::endgroup::"
-      puts "::group::Docker logs"
-      puts `docker compose --file docker-compose-prod.yml logs`
-      puts "::endgroup::"
-      puts "::group::curl output"
-      puts output
-      puts "::endgroup::"
-
-      puts `docker compose logs sidekiq`
-      exit 0
-    end
-  end
+  exit 0
+else
+  puts "::group::Docker logs"
+  puts `docker compose --file docker-compose-prod.yml logs`
+  puts "::endgroup::"
+  puts "::group::curl output"
+  puts output
+  puts "::endgroup::"
+  exit 1
 end
-puts 3
-puts "::group::Docker ps"
-puts `docker compose ps -a`
-puts "::endgroup::"
-puts "::group::Docker logs"
-puts `docker compose --file docker-compose-prod.yml logs`
-puts "::endgroup::"
-puts "::group::curl output"
-puts output
-puts "::endgroup::"
 
-exit 1
