9479b78aa51623fd8f6f1c450322373ab775b6e9
Valentin Ryckaert <valentin.ryckaert@protonmail.com>
2026-07-01 17:23:07 +0200

    feat: when removing a space, if the space is public, keep ressources

--- app/models/space.rb
+++ app/models/space.rb
@@ -5,14 +5,14 @@ class Space < ApplicationRecord
   include LogParameterChanges
 
   belongs_to :user
-  has_many :materials, dependent: :nullify
-  has_many :events, dependent: :nullify
-  has_many :workflows, dependent: :nullify
-  has_many :collections, dependent: :nullify
-  has_many :learning_paths, dependent: :nullify
-  has_many :learning_path_topics, dependent: :nullify
-  has_many :subscriptions, dependent: :nullify
-  has_many :space_roles, dependent: :destroy
+  has_many :materials
+  has_many :events
+  has_many :workflows
+  has_many :collections
+  has_many :learning_paths
+  has_many :learning_path_topics
+  has_many :subscriptions
+  has_many :space_roles
   has_many :space_role_users, through: :space_roles, source: :user, class_name: 'User'
   has_many :administrator_roles, -> { where(key: :admin) }, class_name: 'SpaceRole'
   has_many :administrators, through: :administrator_roles, source: :user, class_name: 'User'
@@ -24,6 +24,8 @@ class Space < ApplicationRecord
   validates :host, presence: true, uniqueness: true, format: /\A[a-zA-Z0-9\-]+(\.[a-zA-Z0-9\-]+)*\z/i
   validates :theme, inclusion: { in: TeSS::Config.themes.keys, allow_blank: true }
   validate :disabled_features_valid?
+
+  before_destroy :handle_associations_on_destroy
 
   has_image(placeholder: TeSS::Config.placeholder['content_provider'])
 
@@ -97,5 +99,36 @@ class Space < ApplicationRecord
         errors.add(:disabled_features, :inclusion)
       end
     end
+  end
+
+  def handle_associations_on_destroy
+    associations = [
+      :materials,
+      :events,
+      :workflows,
+      :collections,
+      :learning_paths,
+      :learning_path_topics,
+      :subscriptions,
+    ]
+
+    associations.each do |relation|
+      records = send(relation)
+
+      if is_private
+        records.delete_all
+      else
+        # explicitly ask Solr to reindex those records so they reappear under the default space.
+        klass = records.klass
+        ids = records.pluck(:id)
+
+        records.update_all(space_id: nil)
+
+        if TeSS::Config.solr_enabled && ids.any? && klass.respond_to?(:solr_index)
+          klass.where(id: ids).solr_index
+        end
+      end
+    end
+    send(:space_roles).delete_all
   end
 end
