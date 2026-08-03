967dbac3e70e08ac948b6813a523cda67a1b19c7
Valentin Ryckaert <valentin.ryckaert@protonmail.com>
2026-08-03 15:14:06 +0200

    fix: fix GroupHelper

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,7 +1,5 @@
 module GroupsHelper
   def groups_info
-    I18n.t('info.groups.description',
-        link: I18n.t('info.groups.link'),
-        url: registering_resources_path(anchor: 'automatic'))
-    end
+    I18n.t('info.groups.description')
+  end
 end
