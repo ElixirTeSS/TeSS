3cf25447ce57b719691bdc82667ef0c404252297
Valentin Ryckaert <valentin.ryckaert@protonmail.com>
2026-08-03 13:31:19 +0200

    feat: add info for groups

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,7 +1,7 @@
 module GroupsHelper
   def groups_info
-    I18n.t('info.events.description',
-        link: I18n.t('info.events.link'),
+    I18n.t('info.groups.description',
+        link: I18n.t('info.groups.link'),
         url: registering_resources_path(anchor: 'automatic'))
     end
 end
