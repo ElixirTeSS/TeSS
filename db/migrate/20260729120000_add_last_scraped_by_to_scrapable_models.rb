9ca197fe4ac30063d0d63f12ca78039650f1bce2
Martin Voigt <m.voigt@hzdr.de>
2026-08-04 16:02:14 +0200

    Remove index: True for last_scraped_by relation as this is already the default

--- db/migrate/20260729120000_add_last_scraped_by_to_scrapable_models.rb
+++ db/migrate/20260729120000_add_last_scraped_by_to_scrapable_models.rb
@@ -1,6 +1,6 @@
 class AddLastScrapedByToScrapableModels < ActiveRecord::Migration[8.1]
   def change
-    add_reference :events, :last_scraped_by, foreign_key: { to_table: :sources, on_delete: :nullify }, index: true
-    add_reference :materials, :last_scraped_by, foreign_key: { to_table: :sources, on_delete: :nullify }, index: true
+    add_reference :events, :last_scraped_by, foreign_key: { to_table: :sources, on_delete: :nullify }
+    add_reference :materials, :last_scraped_by, foreign_key: { to_table: :sources, on_delete: :nullify }
   end
 end
