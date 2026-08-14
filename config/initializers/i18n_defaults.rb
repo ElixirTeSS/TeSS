39cbc5caea7ae12a204b83c2b1e80d01594f930a
kennethrioja <59597207+kennethrioja@users.noreply.github.com>
2026-08-14 14:25:50 +0200

    chore(about): addressing copilot and Finn reviews from #1346 (tags and )

--- config/initializers/i18n_defaults.rb
+++ config/initializers/i18n_defaults.rb
@@ -4,7 +4,8 @@ module I18n
 
     def translate(key, **options)
       defaults = { site_name: TeSS::Config.site['title_short'],
-                    repository: TeSS::Config.site['repository'] }
+                    repository: TeSS::Config.site['repository'],
+                    data_license: TeSS::Config.site['data_license'] }
       original_translate(key, **defaults.merge(options))
     end
     alias t translate
