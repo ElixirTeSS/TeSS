7560b3d5b1598776d19f7bef94845b587457e698
Finn Bacall <finn.bacall@manchester.ac.uk>
2026-08-14 13:45:26 +0100

    Potentially fix system tests where content could not be found on page
    (Even though it is visible in screenshots)

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -4,5 +4,6 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400] do |options|
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-features=DeferRendererTasksAfterInput')
   end
 end
